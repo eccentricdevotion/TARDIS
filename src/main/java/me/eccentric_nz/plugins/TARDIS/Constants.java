a6ab0ce295a5773d441e1b9ccd1a07b27c9eefaa
eccentric devotion <eccentricdevotion@home.nz>
2012-09-05 22:54:05 +1200

    Added help for 'Add' command, added new strings for errors, now getting location and lists from DB

--- src/main/java/me/eccentric_nz/plugins/TARDIS/Constants.java
+++ src/main/java/me/eccentric_nz/plugins/TARDIS/Constants.java
@@ -1,21 +1,18 @@
 package me.eccentric_nz.plugins.TARDIS;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.World;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 
 public class Constants {
 
+    public static TARDISdatabase service = TARDISdatabase.getInstance();
     public static String MY_PLUGIN_NAME;
     public static String TARDIS_KEY;
     public static final String SCHEMATIC_FILE_NAME = "schematic.csv";
@@ -32,7 +29,7 @@ public class Constants {
 
     public enum CMDS {
 
-        CREATE, DELETE, TIMETRAVEL, LIST, SAVE, FIND, ADMIN;
+        CREATE, DELETE, TIMETRAVEL, LIST, SAVE, FIND, ADD, ADMIN;
     }
     public static final String COMMAND_CREATE = ChatColor.BLUE + "Creating a TARDIS\n" + ChatColor.RESET + "You create a TARDIS by placing a " + ChatColor.GOLD + "specific pattern of blocks." + ChatColor.RESET + "\nYou will need to have an IRON BLOCK, a LAPIS BLOCK, and a redstone torch in your inventory.\nYou place the blocks where you want the TARDIS to be, in the following order:\nBottom - IRON BLOCK, middle - LAPIS BLOCK, top - REDSTONE TORCH\nThe TARDIS takes up a 3 x 3 x 4 area (w x d x h), so keep this in mind.\nTo enter the TARDIS, right-click the door with your TARDIS key (by default a redstone torch).";
     public static final String COMMAND_DELETE = ChatColor.BLUE + "Removing a TARDIS\n" + ChatColor.RESET + "To remove your TARDIS, " + ChatColor.GOLD + "break the 'POLICE BOX' wall sign" + ChatColor.RESET + " on the front of the TARDIS.\n" + ChatColor.RED + "WARNING:" + ChatColor.RESET + " You will lose any items you have stored in your TARDIS chest, and any saved time travel destinations.";
@@ -40,11 +37,14 @@ public class Constants {
     public static final String COMMAND_LIST = ChatColor.BLUE + "Listing time travel destinations\n" + ChatColor.RESET + "Simply type " + ChatColor.GOLD + "/TARDIS list" + ChatColor.RESET + "\nto list the destinations saved in the TARDIS console.";
     public static final String COMMAND_SAVE = ChatColor.BLUE + "Saving time travel destinations\n" + ChatColor.RESET + "To save the current TARDIS destination, type\n" + ChatColor.GOLD + "/TARDIS save [slot number] [name]" + ChatColor.RESET + "\nWhere [slot number] is a number from 1 to 3 and [name] is a what you want to call the destination.\n" + ChatColor.RED + "WARNING:" + ChatColor.RESET + " Specifying a slot number that already has a saved destination will cause that destination to be overwritten with the new one.";
     public static final String COMMAND_FIND = ChatColor.BLUE + "Finding the TARDIS\n" + ChatColor.RESET + "Simply type " + ChatColor.GOLD + "/TARDIS find" + ChatColor.RESET + "\nTo display the world name and x, y, z co-ordinates of the last saved location of your TARDIS.";
+    public static final String COMMAND_ADD = ChatColor.BLUE + "Adding companions\n" + ChatColor.RESET + "To allow other players to travel with you in the TARDIS you need to add them to your companions list\nTo do this type: " + ChatColor.GOLD + "/TARDIS add [player]" + ChatColor.RESET + "\nTo remove a companion from the list, type: " + ChatColor.GOLD + "/TARDIS remove [player]" + ChatColor.RESET;
     public static final String COMMAND_ADMIN = ChatColor.BLUE + "TARDIS admin commands\n" + ChatColor.RESET + "Arguments\n" + ChatColor.GOLD + "/TARDIS admin key [Material]" + ChatColor.RESET + " - set the Material used as the TARDIS key. Default: REDSTONE_TORCH_ON.\n" + ChatColor.GOLD + "/TARDIS admin bonus [true|false]" + ChatColor.RESET + " - toggle whether the TARDIS chest is filled with items replaced during the TARDIS construction. Default: true.\n" + ChatColor.GOLD + "/TARDIS admin max_rad [x]" + ChatColor.RESET + " - set the maximum distance (in blocks) you can time travel in the TARDIS. Default: 256\n" + ChatColor.GOLD + "/TARDIS admin protect [true|false]" + ChatColor.RESET + " - set whether the TARDIS blocks will be unaffected by fire/lava. Default: true\n" + ChatColor.GOLD + "/TARDIS admin default [true|false]" + ChatColor.RESET + " - set whether the (inner) TARDIS is created in a specific world. Default: false\n" + ChatColor.GOLD + "/TARDIS admin name [world]" + ChatColor.RESET + " - set the default world the (inner) TARDIS is created in.\n" + ChatColor.GOLD + "/TARDIS admin include [true|false]" + ChatColor.RESET + " - set whether the default world is included in random time travel destinations. Default: false";
     public static final String NO_PERMS_MESSAGE = "You do not have permission to do that!";
-    public static final String NOT_OWNER = "This is not your TARDIS!";
+    public static final String NOT_OWNER = "You are not the Timelord or Companion for this TARDIS!";
     public static final String NO_TARDIS = "You have not created a TARDIS yet!";
     public static final String WRONG_MATERIAL = "The TARDIS key is a ";
+    public static final String TIMELORD_OFFLINE = "The Timelord who owns this TARDIS is offline!";
+    public static final String TIMELORD_NOT_IN = "The Timelord who owns this TARDIS is not inside!";
 
     public static <T extends Enum<T>> T getEnumFromString(Class<T> c, String string) {
         if (c != null && string != null) {
@@ -60,11 +60,10 @@ public class Constants {
         return getEnumFromString(CMDS.class, name);
     }
 
-    public static Location getLocationFromFile(String p, String s, float yaw, float pitch, FileConfiguration c) {
+    public static Location getLocationFromDB(String s, float yaw, float pitch) {
         int savedx = 0, savedy = 0, savedz = 0;
-        // get location from timelords file
-        String saved_loc = c.getString(p + "." + s);
-        String[] data = saved_loc.split(":");
+        // compile location from string
+        String[] data = s.split(":");
         World savedw = Bukkit.getServer().getWorld(data[0]);
         try {
             savedx = Integer.parseInt(data[1]);
@@ -77,73 +76,44 @@ public class Constants {
         return dest;
     }
 
-    public static void list(FileConfiguration c, Player p) {
-        String configPath = p.getName();
-        // construct home string
-        String h = c.getString(configPath + ".home");
-        String[] h_data = h.split(":");
-        p.sendMessage(ChatColor.GREEN + "HOME: " + h_data[0] + " at x:" + h_data[1] + " y:" + h_data[2] + " z:" + h_data[3]);
-        if (c.isSet(configPath + ".dest1")) {
-            String d1 = c.getString(configPath + ".dest1.location");
-            String d1_name = c.getString(configPath + ".dest1.name");
-            String[] d1_data = d1.split(":");
-            p.sendMessage(ChatColor.GREEN + "1. [" + d1_name + "]: " + d1_data[0] + " at x:" + d1_data[1] + " y:" + d1_data[2] + " z:" + d1_data[3]);
-        } else {
-            p.sendMessage(ChatColor.GREEN + "1. No destination saved");
-        }
-        if (c.isSet(configPath + ".dest2")) {
-            String d2 = c.getString(configPath + ".dest2.location");
-            String d2_name = c.getString(configPath + ".dest2.name");
-            String[] d2_data = d2.split(":");
-            p.sendMessage(ChatColor.GREEN + "2. [" + d2_name + "]: " + d2_data[0] + " at x:" + d2_data[1] + " y:" + d2_data[2] + " z:" + d2_data[3]);
-        } else {
-            p.sendMessage(ChatColor.GREEN + "2. No destination saved");
-        }
-        if (c.isSet(configPath + ".dest3")) {
-            String d3 = c.getString(configPath + ".dest3.location");
-            String d3_name = c.getString(configPath + ".dest3.name");
-            String[] d3_data = d3.split(":");
-            p.sendMessage(ChatColor.GREEN + "3. [" + d3_name + "]: " + d3_data[0] + " at x:" + d3_data[1] + " y:" + d3_data[2] + " z:" + d3_data[3]);
-        } else {
-            p.sendMessage(ChatColor.GREEN + "3. No destination saved");
-        }
-    }
-
-    public void removeLineFromFile(File file, String lineToRemove) {
+    public static void list(Player p) {
+        String playerNameStr = p.getName();
+        String querySaves = "SELECT * FROM tardis WHERE owner = '" + playerNameStr + "' LIMIT 1";
         try {
-            //File inFile = new File(file);
-            if (!file.isFile()) {
-                System.out.println(Constants.MY_PLUGIN_NAME + "Parameter is not an existing file");
-                return;
-            }
-            //Construct the new file that will later be renamed to the original filename.
-            File tempFile = new File(file.getAbsolutePath() + ".tmp");
-            BufferedReader br = new BufferedReader(new FileReader(file));
-            BufferedWriter bw = new BufferedWriter(new FileWriter(tempFile));
-            String line;
-            //Read from the original file and write to the new
-            //unless content matches data to be removed.
-            while ((line = br.readLine()) != null) {
-                if (!line.trim().equals(lineToRemove)) {
-                    bw.write(line);
-                    bw.newLine();
+            Connection connection = service.getConnection();
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery(querySaves);
+            if (rs != null && rs.next()) {
+                // construct home string
+                String h = rs.getString("home");
+                String[] h_data = h.split(":");
+                p.sendMessage(ChatColor.GREEN + "HOME: " + h_data[0] + " at x:" + h_data[1] + " y:" + h_data[2] + " z:" + h_data[3]);
+                if (!rs.getString("save1").equals("") && !rs.getString("save1").equals("null") && rs.getString("save1") != null) {
+                    String[] s1 = rs.getString("save1").split("~");
+                    String[] d1_data = s1[1].split(":");
+                    p.sendMessage(ChatColor.GREEN + "1. [" + s1[0] + "]: " + d1_data[0] + " at x:" + d1_data[1] + " y:" + d1_data[2] + " z:" + d1_data[3]);
+                } else {
+                    p.sendMessage(ChatColor.GREEN + "1. No destination saved");
                 }
             }
-            bw.close();
-            br.close();
-            //Delete the original file
-            if (!file.delete()) {
-                System.out.println(Constants.MY_PLUGIN_NAME + "Could not delete file");
-                return;
+            if (!rs.getString("save2").equals("") && !rs.getString("save2").equals("null") && rs.getString("save2") != null) {
+                String[] s2 = rs.getString("save2").split("~");
+                String[] d2_data = s2[1].split(":");
+                p.sendMessage(ChatColor.GREEN + "2. [" + s2[0] + "]: " + d2_data[0] + " at x:" + d2_data[1] + " y:" + d2_data[2] + " z:" + d2_data[3]);
+            } else {
+                p.sendMessage(ChatColor.GREEN + "2. No destination saved");
             }
-            //Rename the new file to the filename the original file had.
-            if (!tempFile.renameTo(file)) {
-                System.out.println(Constants.MY_PLUGIN_NAME + "Could not rename file");
+            if (!rs.getString("save3").equals("") && !rs.getString("save3").equals("null") && rs.getString("save3") != null) {
+                String[] s3 = rs.getString("save3").split("~");
+                String[] d3_data = s3[1].split(":");
+                p.sendMessage(ChatColor.GREEN + "3. [" + s3[0] + "]: " + d3_data[0] + " at x:" + d3_data[1] + " y:" + d3_data[2] + " z:" + d3_data[3]);
+            } else {
+                p.sendMessage(ChatColor.GREEN + "3. No destination saved");
             }
-        } catch (FileNotFoundException ex) {
-            System.err.println(Constants.MY_PLUGIN_NAME + "Chunk file does not exist!" + ex);
-        } catch (IOException ex) {
-            System.err.println(Constants.MY_PLUGIN_NAME + "Could not read chunk file!" + ex);
+            rs.close();
+            statement.close();
+        } catch (SQLException e) {
+            System.err.println(Constants.MY_PLUGIN_NAME + " List Saves Error: " + e);
         }
     }
 }
