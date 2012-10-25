45dde418ff3e8f407d763aa4c65ea89eed593f1d
eccentric devotion <eccentricdevotion@home.nz>
2012-10-25 17:39:14 +1300

    Remove unused import

--- src/main/java/me/eccentric_nz/plugins/TARDIS/TARDISSchematicReader.java
+++ src/main/java/me/eccentric_nz/plugins/TARDIS/TARDISSchematicReader.java
@@ -6,7 +6,6 @@ import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import org.jnbt.ByteArrayTag;
