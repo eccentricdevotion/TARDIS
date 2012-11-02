66fcb64009195c0f8ce167fcce5a94de3685b58d
eccentric devotion <eccentricdevotion@home.nz>
2012-11-02 19:54:35 +1300

    Add method areaCheckInExile()
    Make sure the resultset is not closed before getting all the data we
    need, get the correct substring from the exile area permnission node

--- src/main/java/me/eccentric_nz/plugins/TARDIS/TARDISArea.java
+++ src/main/java/me/eccentric_nz/plugins/TARDIS/TARDISArea.java
@@ -7,7 +7,6 @@ import java.sql.Statement;
 import java.util.Set;
 import org.bukkit.Location;
 import org.bukkit.World;
-import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.permissions.PermissionAttachmentInfo;
 
@@ -39,6 +38,33 @@ public class TARDISArea {
                         chk = false;
                         break;
                     }
+                }
+            }
+            rsArea.close();
+            statement.close();
+        } catch (SQLException e) {
+            System.err.println(Constants.MY_PLUGIN_NAME + " Area block check error: " + e);
+        }
+        return chk;
+    }
+
+    public boolean areaCheckInExile(String a, Location l) {
+        boolean chk = true;
+        try {
+            Connection connection = service.getConnection();
+            Statement statement = connection.createStatement();
+            String queryArea = "SELECT * FROM areas WHERE area_name = '" + a + "'";
+            ResultSet rsArea = statement.executeQuery(queryArea);
+            if (rsArea.next()) {
+                String w = rsArea.getString("world");
+                String lw = l.getWorld().getName();
+                int minx = rsArea.getInt("minx");
+                int minz = rsArea.getInt("minz");
+                int maxx = rsArea.getInt("maxx");
+                int maxz = rsArea.getInt("maxz");
+                // is clicked block within a defined TARDIS area?
+                if (w.equals(lw) && (l.getX() <= maxx && l.getZ() <= maxz && l.getX() >= minx && l.getZ() >= minz)) {
+                    chk = false;
                 }
             }
             rsArea.close();
@@ -91,39 +117,45 @@ public class TARDISArea {
         try {
             Connection connection = service.getConnection();
             Statement statement = connection.createStatement();
-            //String w = l.getWorld().getName();
             String queryArea = "SELECT * FROM areas WHERE area_name = '" + a + "'";
             ResultSet rsArea = statement.executeQuery(queryArea);
-            int minx = rsArea.getInt("minx");
-            int x = minx + 2;
-            int minz = rsArea.getInt("minz");
-            int z = minz + 2;
-            int maxx = rsArea.getInt("maxx");
-            int maxz = rsArea.getInt("maxz");
-            boolean chk = false;
-            while (chk == false) {
-                String queryLoc = "SELECT save FROM tardis WHERE save LIKE '" + rsArea.getString("world") + ":" + x + ":%:" + z + "'";
-                ResultSet rsLoc = statement.executeQuery(queryLoc);
-                if (rsLoc.next()) {
-                    if (x <= maxx) {
-                        x += 5;
-                    } else {
-                        x = minx + 2;
-                        if (z <= maxz) {
-                            z += 5;
+            if (rsArea.next()) {
+                int minx = rsArea.getInt("minx");
+                int x = minx + 2;
+                int minz = rsArea.getInt("minz");
+                int z = minz + 2;
+                int maxx = rsArea.getInt("maxx");
+                int maxz = rsArea.getInt("maxz");
+                String wStr = rsArea.getString("world");
+                rsArea.close();
+                boolean chk = false;
+                while (chk == false) {
+                    String queryLoc = "SELECT save FROM tardis WHERE save LIKE '" + wStr + ":" + x + ":%:" + z + "'";
+                    ResultSet rsLoc = statement.executeQuery(queryLoc);
+                    if (rsLoc.next()) {
+                        if (x <= maxx) {
+                            x += 5;
                         } else {
-                            z = minz + 2;
+                            x = minx + 2;
+                            if (z <= maxz) {
+                                z += 5;
+                            } else {
+                                z = minz + 2;
+                            }
                         }
+                    } else {
+                        System.out.println("Found a spot");
+                        chk = true;
+                        rsLoc.close();
+                        statement.close();
+                        break;
                     }
-                } else {
-                    chk = true;
-                    break;
                 }
-            }
-            if (chk == true) {
-                World w = plugin.getServer().getWorld(rsArea.getString("world"));
-                int y = w.getHighestBlockYAt(x, z);
-                location = w.getBlockAt(x, y, z).getLocation();
+                if (chk == true) {
+                    World w = plugin.getServer().getWorld(wStr);
+                    int y = w.getHighestBlockYAt(x, z);
+                    location = w.getBlockAt(x, y, z).getLocation();
+                }
             }
         } catch (SQLException e) {
             System.err.println(Constants.MY_PLUGIN_NAME + " Area parking error: " + e);
@@ -135,9 +167,9 @@ public class TARDISArea {
         Set<PermissionAttachmentInfo> perms = p.getEffectivePermissions();
         String area = "";
         for (PermissionAttachmentInfo pai : perms) {
-            //System.out.println(pai.getPermission());
             if (pai.getPermission().contains("tardis.area")) {
-                area = pai.getPermission();
+                int len = pai.getPermission().length();
+                area = pai.getPermission().substring(12, len);
             }
         }
         return area;
