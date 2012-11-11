16ffe4b02d8e13f122eda33d7cb3bb81ec7e4c3f
eccentric devotion <eccentricdevotion@home.nz>
2012-11-11 19:00:04 +1300

    Cancel BlockIgniteEvent on TARDIS Police Box

--- src/main/java/me/eccentric_nz/plugins/TARDIS/TARDISBlockProtectListener.java
+++ src/main/java/me/eccentric_nz/plugins/TARDIS/TARDISBlockProtectListener.java
@@ -1,5 +1,12 @@
 package me.eccentric_nz.plugins.TARDIS;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -10,18 +17,43 @@ import org.bukkit.event.block.BlockIgniteEvent;
 public class TARDISBlockProtectListener implements Listener {
 
     private TARDIS plugin;
+    TARDISdatabase service = TARDISdatabase.getInstance();
+    List<BlockFace> faces = new ArrayList<BlockFace>();
 
     public TARDISBlockProtectListener(TARDIS plugin) {
         this.plugin = plugin;
+        faces.add(BlockFace.UP);
+        faces.add(BlockFace.DOWN);
+        faces.add(BlockFace.NORTH);
+        faces.add(BlockFace.SOUTH);
+        faces.add(BlockFace.EAST);
+        faces.add(BlockFace.WEST);
     }
 
     @EventHandler(priority = EventPriority.LOWEST)
     public void onBlockIgnite(BlockIgniteEvent event) {
+        Block b = event.getBlock();
+        try {
+            Connection connection = service.getConnection();
+            Statement statement = connection.createStatement();
+            for (BlockFace bf : faces) {
+                Block chkBlock = b.getRelative(bf);
+                String l = chkBlock.getLocation().toString();
+                String queryBlock = "SELECT location FROM blocks WHERE location = '" + l + "'";
+                ResultSet rsBlockLoc = statement.executeQuery(queryBlock);
+                if (rsBlockLoc.next()) {
+                    event.setCancelled(true);
+                    break;
+                }
+            }
+        } catch (SQLException e) {
+            System.err.println(Constants.MY_PLUGIN_NAME + " Could not get block locations from DB!");
+        }
         if (plugin.config.getBoolean("protect_blocks") == true) {
             String[] set = {"EAST", "SOUTH", "WEST", "NORTH", "UP", "DOWN"};
             for (String f : set) {
-                int id = event.getBlock().getRelative(BlockFace.valueOf(f)).getTypeId();
-                byte d = event.getBlock().getRelative(BlockFace.valueOf(f)).getData();
+                int id = b.getRelative(BlockFace.valueOf(f)).getTypeId();
+                byte d = b.getRelative(BlockFace.valueOf(f)).getData();
                 if (id == 35 && (d == 1 || d == 7 || d == 8)) {
                     event.setCancelled(true);
                     break;
@@ -32,11 +64,24 @@ public class TARDISBlockProtectListener implements Listener {
 
     @EventHandler(priority = EventPriority.LOWEST)
     public void onBlockBurn(BlockBurnEvent event) {
+        Block b = event.getBlock();
+        String l = b.getLocation().toString();
+        try {
+            Connection connection = service.getConnection();
+            Statement statement = connection.createStatement();
+            String queryBlock = "SELECT location FROM blocks WHERE location = '" + l + "'";
+            ResultSet rsBlockLoc = statement.executeQuery(queryBlock);
+            if (rsBlockLoc.next()) {
+                event.setCancelled(true);
+            }
+        } catch (SQLException e) {
+            System.err.println(Constants.MY_PLUGIN_NAME + " Could not get block locations from DB!");
+        }
+
         if (plugin.config.getBoolean("protect_blocks") == true) {
-            String[] set = {"EAST", "SOUTH", "WEST", "NORTH", "UP", "DOWN"};
-            for (String f : set) {
-                int id = event.getBlock().getRelative(BlockFace.valueOf(f)).getTypeId();
-                byte d = event.getBlock().getRelative(BlockFace.valueOf(f)).getData();
+            for (BlockFace bf : faces) {
+                int id = b.getRelative(bf).getTypeId();
+                byte d = b.getRelative(bf).getData();
                 if (id == 35 && (d == 1 || d == 7 || d == 8)) {
                     event.setCancelled(true);
                     break;
