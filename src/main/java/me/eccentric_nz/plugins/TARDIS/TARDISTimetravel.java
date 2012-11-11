d396e625fe14b6fd76a2a826c52e5d0eb193a484
eccentric devotion <eccentricdevotion@home.nz>
2012-11-11 21:35:34 +1300

    Add methods to find safe Nether location

--- src/main/java/me/eccentric_nz/plugins/TARDIS/TARDISTimetravel.java
+++ src/main/java/me/eccentric_nz/plugins/TARDIS/TARDISTimetravel.java
@@ -39,17 +39,15 @@ public class TARDISTimetravel {
         List<World> normalWorlds = new ArrayList<World>();
         for (String o : worldlist) {
             Environment env = plugin.getServer().getWorld(o).getEnvironment();
-            if (env == Environment.NORMAL || (env == Environment.THE_END && plugin.config.getBoolean("the_end") == true)) {
-                if (plugin.config.getBoolean("include_default_world") == Boolean.valueOf("true")
-                        || plugin.config.getBoolean("default_world") == Boolean.valueOf("false")) {
+            if (plugin.config.getBoolean("include_default_world") == Boolean.valueOf("true")
+                    || plugin.config.getBoolean("default_world") == Boolean.valueOf("false")) {
+                if (plugin.config.getBoolean("worlds." + o) == Boolean.valueOf("true")) {
+                    normalWorlds.add(plugin.getServer().getWorld(o));
+                }
+            } else {
+                if (!o.equals(plugin.config.getString("default_world_name"))) {
                     if (plugin.config.getBoolean("worlds." + o) == Boolean.valueOf("true")) {
                         normalWorlds.add(plugin.getServer().getWorld(o));
-                    }
-                } else {
-                    if (!o.equals(plugin.config.getString("default_world_name"))) {
-                        if (plugin.config.getBoolean("worlds." + o) == Boolean.valueOf("true")) {
-                            normalWorlds.add(plugin.getServer().getWorld(o));
-                        }
                     }
                 }
             }
@@ -69,6 +67,16 @@ public class TARDISTimetravel {
             wgchk = new TARDISWorldGuardChecker(plugin);
         }
         TARDISArea ta = new TARDISArea(plugin);
+        if (randworld.getEnvironment().equals(Environment.NETHER)) {
+            while (danger == true) {
+                wherex = randomX(rand, range, quarter, rx, ry, max);
+                wherez = randomZ(rand, range, quarter, rz, ry, max);
+                if (safeNether(randworld, wherex, wherez, d)) {
+                    danger = false;
+                    break;
+                }
+            }
+        }
         if (randworld.getEnvironment().equals(Environment.THE_END)) {
             while (danger == true) {
                 wherex = rand.nextInt(240);
@@ -110,51 +118,13 @@ public class TARDISTimetravel {
                 }
             }
             dest = new Location(randworld, wherex, highest, wherez);
-        } else {
+        }
+        if (randworld.getEnvironment().equals(Environment.NORMAL)) {
             while (danger == true) {
                 count = 0;
-                wherex = rand.nextInt(range);
-                wherez = rand.nextInt(range);
-                // add the distance from the x and z repeaters
-                if (rx >= 4 && rx <= 7) {
-                    wherex += (quarter);
-                }
-                if (rx >= 8 && rx <= 11) {
-                    wherex += (quarter * 2);
-                }
-                if (rx >= 12 && rx <= 15) {
-                    wherex += (quarter * 3);
-                }
-                if (rz >= 4 && rz <= 7) {
-                    wherez += (quarter);
-                }
-                if (rz >= 8 && rz <= 11) {
-                    wherez += (quarter * 2);
-                }
-                if (rz >= 12 && rz <= 15) {
-                    wherez += (quarter * 3);
-                }
-
-                // add chance of negative values
-                wherex = wherex * 2;
-                wherez = wherez * 2;
-                wherex = wherex - max;
-                wherez = wherez - max;
-
-                // use multiplier based on position of third repeater
-                if (ry >= 4 && ry <= 7) {
-                    wherex = wherex * 2;
-                    wherez = wherez * 2;
-                }
-                if (ry >= 8 && ry <= 11) {
-                    wherex = wherex * 3;
-                    wherez = wherez * 3;
-                }
-                if (ry >= 12 && ry <= 15) {
-                    wherex = wherex * 4;
-                    wherez = wherez * 4;
-                }
-
+                // randomX(Random rand, int range, int quarter, byte rx, byte ry, int max)
+                wherex = randomX(rand, range, quarter, rx, ry, max);
+                wherez = randomZ(rand, range, quarter, rz, ry, max);
                 highest = randworld.getHighestBlockYAt(wherex, wherez);
                 if (highest > 3) {
                     Block currentBlock = randworld.getBlockAt(wherex, highest, wherez);
@@ -229,7 +199,7 @@ public class TARDISTimetravel {
 
     private boolean isItSafe(int id) {
         boolean safe = true;
-        if (id == 0 || id == 6 || id == 31 || id == 32 || id == 37 || id == 38 || id == 39 || id == 40 || id == 78) {
+        if (id == 0 || id == 6 || id == 31 || id == 32 || id == 37 || id == 38 || id == 39 || id == 40 || id == 78 || id == 115) {
             safe = false;
         }
         return safe;
@@ -259,5 +229,102 @@ public class TARDISTimetravel {
                 break;
         }
         return startLoc;
+    }
+
+    private boolean safeNether(World nether, int wherex, int wherez, Constants.COMPASS d) {
+        boolean safe = false;
+        int startx, starty, startz, resetx, resetz, count;
+        Random rand = new Random();
+        int wherey = 100;
+        Block startBlock = nether.getBlockAt(wherex, wherey, wherez);
+        while (startBlock.getTypeId() != 0) {
+            startBlock = startBlock.getRelative(BlockFace.DOWN);
+        }
+        int air = 0;
+        while (startBlock.getTypeId() == 0 && startBlock.getLocation().getBlockY() > 30) {
+            startBlock = startBlock.getRelative(BlockFace.DOWN);
+            air++;
+        }
+        int id = startBlock.getTypeId();
+        if ((id == 87 || id == 88 || id == 89 || id == 112 || id == 113 || id == 114) && air >= 4) {
+            Location netherLocation = startBlock.getLocation();
+            int netherLocY = netherLocation.getBlockY();
+            netherLocation.setY(netherLocY + 1);
+            // get start location for checking there is enough space
+            int gsl[] = getStartLocation(netherLocation, d);
+            startx = gsl[0];
+            resetx = gsl[1];
+            starty = netherLocation.getBlockY();
+            startz = gsl[2];
+            resetz = gsl[3];
+            count = safeLocation(startx, starty, startz, resetx, resetz, nether, d);
+            if (count == 0) {
+                safe = true;
+                dest = netherLocation;
+            }
+        }
+        return safe;
+    }
+
+    private int randomX(Random rand, int range, int quarter, byte rx, byte ry, int max) {
+        int wherex;
+        wherex = rand.nextInt(range);
+        // add the distance from the x and z repeaters
+        if (rx >= 4 && rx <= 7) {
+            wherex += (quarter);
+        }
+        if (rx >= 8 && rx <= 11) {
+            wherex += (quarter * 2);
+        }
+        if (rx >= 12 && rx <= 15) {
+            wherex += (quarter * 3);
+        }
+
+        // add chance of negative values
+        wherex = wherex * 2;
+        wherex = wherex - max;
+
+        // use multiplier based on position of third repeater
+        if (ry >= 4 && ry <= 7) {
+            wherex = wherex * 2;
+        }
+        if (ry >= 8 && ry <= 11) {
+            wherex = wherex * 3;
+        }
+        if (ry >= 12 && ry <= 15) {
+            wherex = wherex * 4;
+        }
+        return wherex;
+    }
+
+    private int randomZ(Random rand, int range, int quarter, byte rz, byte ry, int max) {
+        int wherez;
+        wherez = rand.nextInt(range);
+        // add the distance from the x and z repeaters
+        if (rz >= 4 && rz <= 7) {
+            wherez += (quarter);
+        }
+        if (rz >= 8 && rz <= 11) {
+            wherez += (quarter * 2);
+        }
+        if (rz >= 12 && rz <= 15) {
+            wherez += (quarter * 3);
+        }
+
+        // add chance of negative values
+        wherez = wherez * 2;
+        wherez = wherez - max;
+
+        // use multiplier based on position of third repeater
+        if (ry >= 4 && ry <= 7) {
+            wherez = wherez * 2;
+        }
+        if (ry >= 8 && ry <= 11) {
+            wherez = wherez * 3;
+        }
+        if (ry >= 12 && ry <= 15) {
+            wherez = wherez * 4;
+        }
+        return wherez;
     }
 }
