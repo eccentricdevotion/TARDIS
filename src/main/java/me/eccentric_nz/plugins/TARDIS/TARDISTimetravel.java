ea65ffc02fac0c1175f8a41ed2886ddc5f74dd0f
eccentric devotion <eccentricdevotion@home.nz>
2012-12-08 21:43:50 +1300

    Limit the time allowed to find a safe location, if it times out, set Y to cloud height (configuarble)
    Fixes skyblock worlds

--- src/main/java/me/eccentric_nz/plugins/TARDIS/TARDISTimetravel.java
+++ src/main/java/me/eccentric_nz/plugins/TARDIS/TARDISTimetravel.java
@@ -136,46 +136,53 @@ public class TARDISTimetravel {
             dest = new Location(randworld, wherex, highest, wherez);
         }
         if (randworld.getEnvironment().equals(Environment.NORMAL)) {
+            long timeout = System.currentTimeMillis() + (plugin.config.getLong("timeout")*1000);
             while (danger == true) {
-                count = 0;
-                // randomX(Random rand, int range, int quarter, byte rx, byte ry, int max)
-                wherex = randomX(rand, range, quarter, rx, ry, max);
-                wherez = randomZ(rand, range, quarter, rz, ry, max);
-                highest = randworld.getHighestBlockYAt(wherex, wherez);
-                if (highest > 3) {
-                    Block currentBlock = randworld.getBlockAt(wherex, highest, wherez);
-                    if ((currentBlock.getTypeId() == 8 || currentBlock.getTypeId() == 9) && plugin.config.getBoolean("land_on_water") == false) {
-                        count = 1;
-                    } else {
-                        if (currentBlock.getType() == Material.AIR || currentBlock.getType() == Material.SNOW || currentBlock.getType() == Material.LONG_GRASS || currentBlock.getType() == Material.RED_ROSE || currentBlock.getType() == Material.YELLOW_FLOWER || currentBlock.getType() == Material.BROWN_MUSHROOM || currentBlock.getType() == Material.RED_MUSHROOM || currentBlock.getType() == Material.SAPLING || currentBlock.getType() == Material.SNOW) {
-                            currentBlock = currentBlock.getRelative(BlockFace.DOWN);
-                        }
-                        Location chunk_loc = currentBlock.getLocation();
-                        if (plugin.WorldGuardOnServer && plugin.wgchk.cantBuild(p, chunk_loc) && plugin.config.getBoolean("respect_worldguard")) {
+                if (System.currentTimeMillis() < timeout) {
+                    count = 0;
+                    // randomX(Random rand, int range, int quarter, byte rx, byte ry, int max)
+                    wherex = randomX(rand, range, quarter, rx, ry, max);
+                    wherez = randomZ(rand, range, quarter, rz, ry, max);
+                    highest = randworld.getHighestBlockYAt(wherex, wherez);
+                    if (highest > 3) {
+                        Block currentBlock = randworld.getBlockAt(wherex, highest, wherez);
+                        if ((currentBlock.getTypeId() == 8 || currentBlock.getTypeId() == 9) && plugin.config.getBoolean("land_on_water") == false) {
                             count = 1;
-                        }
-                        if (plugin.ta.areaCheckLocPlayer(p, chunk_loc)) {
-                            plugin.trackPerm.remove(p.getName());
-                            count = 1;
-                        }
-                        randworld.getChunkAt(chunk_loc).load();
-                        randworld.getChunkAt(chunk_loc).load(true);
-                        while (!randworld.getChunkAt(chunk_loc).isLoaded()) {
+                        } else {
+                            if (currentBlock.getType() == Material.AIR || currentBlock.getType() == Material.SNOW || currentBlock.getType() == Material.LONG_GRASS || currentBlock.getType() == Material.RED_ROSE || currentBlock.getType() == Material.YELLOW_FLOWER || currentBlock.getType() == Material.BROWN_MUSHROOM || currentBlock.getType() == Material.RED_MUSHROOM || currentBlock.getType() == Material.SAPLING || currentBlock.getType() == Material.SNOW) {
+                                currentBlock = currentBlock.getRelative(BlockFace.DOWN);
+                            }
+                            Location chunk_loc = currentBlock.getLocation();
+                            if (plugin.WorldGuardOnServer && plugin.wgchk.cantBuild(p, chunk_loc) && plugin.config.getBoolean("respect_worldguard")) {
+                                count = 1;
+                            }
+                            if (plugin.ta.areaCheckLocPlayer(p, chunk_loc)) {
+                                plugin.trackPerm.remove(p.getName());
+                                count = 1;
+                            }
                             randworld.getChunkAt(chunk_loc).load();
+                            randworld.getChunkAt(chunk_loc).load(true);
+                            while (!randworld.getChunkAt(chunk_loc).isLoaded()) {
+                                randworld.getChunkAt(chunk_loc).load();
+                            }
+                            // get start location for checking there is enough space
+                            int gsl[] = getStartLocation(chunk_loc, d);
+                            startx = gsl[0];
+                            resetx = gsl[1];
+                            starty = chunk_loc.getBlockY() + 1;
+                            startz = gsl[2];
+                            resetz = gsl[3];
+                            count = safeLocation(startx, starty, startz, resetx, resetz, randworld, d);
                         }
-                        // get start location for checking there is enough space
-                        int gsl[] = getStartLocation(chunk_loc, d);
-                        startx = gsl[0];
-                        resetx = gsl[1];
-                        starty = chunk_loc.getBlockY() + 1;
-                        startz = gsl[2];
-                        resetz = gsl[3];
-                        count = safeLocation(startx, starty, startz, resetx, resetz, randworld, d);
+                    } else {
+                        count = 1;
+                    }
+                    if (count == 0) {
+                        danger = false;
+                        break;
                     }
                 } else {
-                    count = 1;
-                }
-                if (count == 0) {
+                    highest = plugin.config.getInt("timeout_height");
                     danger = false;
                     break;
                 }
