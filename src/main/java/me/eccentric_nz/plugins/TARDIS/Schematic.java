5001f6c754d068d56691d340cb6d82ee0541a5e3
eccentric devotion <eccentricdevotion@home.nz>
2012-12-19 17:08:22 +1300

    Make sure the line is not null before trying to process, add finally to close buffered reader

--- src/main/java/me/eccentric_nz/plugins/TARDIS/Schematic.java
+++ src/main/java/me/eccentric_nz/plugins/TARDIS/Schematic.java
@@ -17,21 +17,31 @@ public class Schematic {
     public static String[][][] schematic(File file, short h, short w, short l) {
 
         // load data from csv file
+        BufferedReader bufRdr = null;
         try {
             blocks = new String[h][w][l];
 
-            BufferedReader bufRdr = new BufferedReader(new FileReader(file));
+            bufRdr = new BufferedReader(new FileReader(file));
             String line;
             //read each line of text file
             for (int level = 0; level < h; level++) {
                 for (int row = 0; row < w; row++) {
                     line = bufRdr.readLine();
-                    String[] strArr = line.split(",");
-                    System.arraycopy(strArr, 0, blocks[level][row], 0, l);
+                    if (line != null) {
+                        String[] strArr = line.split(",");
+                        System.arraycopy(strArr, 0, blocks[level][row], 0, l);
+                    }
                 }
             }
         } catch (IOException io) {
             System.err.println(Constants.MY_PLUGIN_NAME + " Could not read csv file");
+        } finally {
+            try {
+                if (bufRdr != null) {
+                    bufRdr.close();
+                }
+            } catch (Exception e) {
+            }
         }
         return blocks;
     }
