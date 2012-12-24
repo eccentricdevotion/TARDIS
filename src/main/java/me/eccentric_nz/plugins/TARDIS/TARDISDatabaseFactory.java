bab7baf5fddb8c79bbc567e37cf54aabe8b8b6ab
eccentric devotion <eccentricdevotion@home.nz>
2012-12-25 10:01:05 +1300

    New database class for query building

---
+++ src/main/java/me/eccentric_nz/plugins/TARDIS/TARDISDatabaseFactory.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package me.eccentric_nz.plugins.TARDIS;
+
+/**
+ *
+ * @author rob
+ */
+public class TARDISDatabaseFactory {
+
+}
