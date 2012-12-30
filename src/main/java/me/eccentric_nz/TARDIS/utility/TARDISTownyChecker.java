cd9a3c018ea6d89c3453ebd9345cfad5232dc13c
eccentric devotion <eccentricdevotion@home.nz>
2012-12-30 14:52:23 +1300

    Class to check whether a location is in the wilderness and not a Towny town

---
+++ src/main/java/me/eccentric_nz/TARDIS/utility/TARDISTownyChecker.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2012 eccentric_nz
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package me.eccentric_nz.TARDIS.utility;
+
+import com.palmergames.bukkit.towny.Towny;
+import com.palmergames.bukkit.towny.object.Coord;
+import com.palmergames.bukkit.towny.object.WorldCoord;
+import me.eccentric_nz.TARDIS.TARDIS;
+import org.bukkit.Location;
+
+/**
+ *
+ * @author eccentric_nz
+ */
+public class TARDISTownyChecker {
+
+    private TARDIS plugin;
+    private Towny towny;
+
+    public TARDISTownyChecker(TARDIS plugin) {
+        this.plugin = plugin;
+        if (plugin.townyOnServer) {
+            towny = (Towny) plugin.getServer().getPluginManager().getPlugin("Towny");
+        }
+    }
+
+    /**
+     * Checks whether a location is in 'wilderness'... ie NOT in a Towny town.
+     *
+     * @param l the location instance to check.
+     */
+    public boolean isWilderness(Location l) {
+        boolean bool = true;
+        if (towny != null) {
+            try {
+                //could be block Coord.parseCoord(Block block);
+                Coord cord = Coord.parseCoord(l.getBlockX(), l.getBlockZ());
+                WorldCoord wcoord = new WorldCoord(l.getWorld().getName(), cord);
+                if (wcoord.getTownBlock().hasTown()) {
+                    bool = false;
+                }
+            } catch (Exception e) {
+                plugin.debug("Towny checker error " + e.getMessage());
+            }
+        }
+        return bool;
+    }
+}
