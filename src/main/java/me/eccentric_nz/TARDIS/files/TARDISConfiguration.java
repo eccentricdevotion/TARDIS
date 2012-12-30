f868748ea7374ae03931f62f4c5fc4144971ad8c
eccentric devotion <eccentricdevotion@home.nz>
2012-12-30 14:52:58 +1300

    Add new border_radius option

--- src/main/java/me/eccentric_nz/TARDIS/files/TARDISConfiguration.java
+++ src/main/java/me/eccentric_nz/TARDIS/files/TARDISConfiguration.java
@@ -44,6 +44,7 @@ public class TARDISConfiguration {
         intOptions.put("timeout_height", 135);
         intOptions.put("timeout", 5);
         intOptions.put("tp_radius", 256);
+        intOptions.put("border_radius", 256);
         // string
         strOptions.put("default_world_name", "myridiculouslylongworldnameiscalledcuthbert");
         strOptions.put("key", "STICK");
