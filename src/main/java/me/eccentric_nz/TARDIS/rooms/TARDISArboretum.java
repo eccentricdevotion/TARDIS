87bcbcfbc5eb9440797882e0b004866ccfb2c4db
eccentric devotion <eccentricdevotion@home.nz>
2012-12-30 19:35:50 +1300

    New package and classes for TARDIS rooms

---
+++ src/main/java/me/eccentric_nz/TARDIS/rooms/TARDISArboretum.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2012 eccentric_nz
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package me.eccentric_nz.TARDIS.rooms;
+
+import me.eccentric_nz.TARDIS.TARDIS;
+import me.eccentric_nz.TARDIS.database.TARDISDatabase;
+
+/**
+ *
+ * @author eccentric_nz
+ */
+public class TARDISArboretum {
+
+    private final TARDIS plugin;
+    TARDISDatabase service = TARDISDatabase.getInstance();
+
+    public TARDISArboretum(TARDIS plugin) {
+        this.plugin = plugin;
+    }
+
+}
