a538ad01ff1c6d1619c58f12d9f1a4118be23ba9
eccentric devotion <eccentricdevotion@home.nz>
2013-01-02 16:18:51 +1300

    Build a passage

--- src/main/java/me/eccentric_nz/TARDIS/rooms/TARDISPassage.java
+++ src/main/java/me/eccentric_nz/TARDIS/rooms/TARDISPassage.java
@@ -14,11 +14,12 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package me.eccentric_nz.TARDIS.rooms;
 
+import java.util.HashMap;
 import me.eccentric_nz.TARDIS.TARDIS;
 import me.eccentric_nz.TARDIS.database.TARDISDatabase;
+import org.bukkit.Location;
 
 /**
  *
@@ -27,10 +28,59 @@ import me.eccentric_nz.TARDIS.database.TARDISDatabase;
 public class TARDISPassage {
 
     private final TARDIS plugin;
+    private Location l;
+    private int middle_id;
+    private byte middle_data;
     TARDISDatabase service = TARDISDatabase.getInstance();
 
-    public TARDISPassage(TARDIS plugin) {
+    public TARDISPassage(TARDIS plugin, Location l, int middle_id, byte middle_data) {
         this.plugin = plugin;
+        this.l = l;
+        this.middle_id = middle_id;
+        this.middle_data = middle_data;
     }
 
+    public void passage() {
+        String[][][] s;
+        short h, w, l;
+        s = plugin.passageschematic;
+        h = plugin.passagedimensions[0];
+        w = plugin.passagedimensions[1];
+        l = plugin.passagedimensions[2];
+        int level, row, col, id, x, y, z, startx, starty = 15, startz, resetx, resetz;
+        String tmp;
+        byte data;
+        for (level = 0; level < h; level++) {
+            for (row = 0; row < w; row++) {
+                for (col = 0; col < l; col++) {
+                    tmp = s[level][row][col];
+                    if (!tmp.equals("0:0")) {
+                        if (tmp.contains(":")) {
+                            String[] iddata = tmp.split(":");
+                            id = plugin.utils.parseNum(iddata[0]);
+                            data = Byte.parseByte(iddata[1]);
+                            if (id == 35 && data == 1) {
+                                switch (middle_id) {
+                                    case 22:
+                                        break;
+                                    default:
+                                        id = middle_id;
+                                        data = middle_data;
+                                }
+                            }
+                        } else {
+                            id = plugin.utils.parseNum(tmp);
+                            data = 0;
+                        }
+
+                    }
+                    startx += x;
+                }
+                startx = resetx;
+                startz += z;
+            }
+            startz = resetz;
+            starty += 1;
+        }
+    }
 }
