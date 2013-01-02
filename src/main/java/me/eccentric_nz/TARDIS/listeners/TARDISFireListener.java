3f2d78e21dc4ba4c0ed07e53401476197560b8cb
eccentric devotion <eccentricdevotion@home.nz>
2013-01-03 00:52:34 +1300

    Will remove this class eventually, but remove protect blocks for now - WorldGuard should take care of it

--- src/main/java/me/eccentric_nz/TARDIS/listeners/TARDISFireListener.java
+++ src/main/java/me/eccentric_nz/TARDIS/listeners/TARDISFireListener.java
@@ -70,17 +70,17 @@ public class TARDISFireListener implements Listener {
                 break;
             }
         }
-        if (plugin.getConfig().getBoolean("protect_blocks") == true) {
-            String[] set = {"EAST", "SOUTH", "WEST", "NORTH", "UP", "DOWN"};
-            for (String f : set) {
-                int id = b.getRelative(BlockFace.valueOf(f)).getTypeId();
-                byte d = b.getRelative(BlockFace.valueOf(f)).getData();
-                if (id == 35 && (d == 1 || d == 7 || d == 8)) {
-                    event.setCancelled(true);
-                    break;
-                }
-            }
-        }
+//        if (plugin.getConfig().getBoolean("protect_blocks") == true) {
+//            String[] set = {"EAST", "SOUTH", "WEST", "NORTH", "UP", "DOWN"};
+//            for (String f : set) {
+//                int id = b.getRelative(BlockFace.valueOf(f)).getTypeId();
+//                byte d = b.getRelative(BlockFace.valueOf(f)).getData();
+//                if (id == 35 && (d == 1 || d == 7 || d == 8)) {
+//                    event.setCancelled(true);
+//                    break;
+//                }
+//            }
+//        }
     }
 
     @EventHandler(priority = EventPriority.LOWEST)
@@ -93,15 +93,15 @@ public class TARDISFireListener implements Listener {
         if (rs.resultSet()) {
             event.setCancelled(true);
         }
-        if (plugin.getConfig().getBoolean("protect_blocks") == true) {
-            for (BlockFace bf : faces) {
-                int id = b.getRelative(bf).getTypeId();
-                byte d = b.getRelative(bf).getData();
-                if (id == 35 && (d == 1 || d == 7 || d == 8)) {
-                    event.setCancelled(true);
-                    break;
-                }
-            }
-        }
+//        if (plugin.getConfig().getBoolean("protect_blocks") == true) {
+//            for (BlockFace bf : faces) {
+//                int id = b.getRelative(bf).getTypeId();
+//                byte d = b.getRelative(bf).getData();
+//                if (id == 35 && (d == 1 || d == 7 || d == 8)) {
+//                    event.setCancelled(true);
+//                    break;
+//                }
+//            }
+//        }
     }
 }
