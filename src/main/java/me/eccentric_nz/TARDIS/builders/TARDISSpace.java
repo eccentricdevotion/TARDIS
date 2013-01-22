2ad3ceac83ef2c39f51b3af96667edad30f0158e
eccentric devotion <eccentricdevotion@home.nz>
2013-01-22 17:29:14 +1300

    It's a NORMAL world now, not THE_END

--- src/main/java/me/eccentric_nz/TARDIS/builders/TARDISSpace.java
+++ src/main/java/me/eccentric_nz/TARDIS/builders/TARDISSpace.java
@@ -54,7 +54,7 @@ public class TARDISSpace {
             plugin.getConfig().set("worlds:" + name, false);
             plugin.saveConfig();
             if (plugin.pm.isPluginEnabled("Multiverse-Core")) {
-                plugin.getServer().dispatchCommand(plugin.console, "mv import " + name + " the_end TARDISChunkGenerator");
+                plugin.getServer().dispatchCommand(plugin.console, "mv import " + name + " normal TARDISChunkGenerator");
             }
             if (plugin.pm.isPluginEnabled("WorldBorder")) {
                 // wb <world> set <radius> <x> <z>
