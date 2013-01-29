4487e9c2384afd9fa3bedd8b5022813b5eb3c3df
eccentric devotion <eccentricdevotion@home.nz>
2013-01-30 00:34:47 +1300

    Fix /tardisadmin config command

--- src/main/java/me/eccentric_nz/TARDIS/commands/TARDISAdminCommands.java
+++ src/main/java/me/eccentric_nz/TARDIS/commands/TARDISAdminCommands.java
@@ -168,23 +168,34 @@ public class TARDISAdminCommands implements CommandExecutor {
                         sender.sendMessage(plugin.pluginName + ChatColor.RED + " Here are the current plugin config options!");
                         for (String cname : configNames) {
                             String value = plugin.getConfig().getString(cname);
-                            if (cname.equals("worlds")) {
-                                sender.sendMessage(ChatColor.AQUA + cname + ":" + ChatColor.RESET);
-                                Set<String> worldNames = plugin.getConfig().getConfigurationSection("worlds").getKeys(false);
-                                for (String wname : worldNames) {
-                                    String enabled = plugin.getConfig().getString("worlds." + wname);
-                                    sender.sendMessage("      " + ChatColor.GREEN + wname + ": " + ChatColor.RESET + enabled);
+                            if (cname.equals("worlds") || cname.equals("rooms") || cname.equals("rechargers")) {
+                                if (cname.equals("worlds")) {
+                                    sender.sendMessage(ChatColor.AQUA + cname + ":" + ChatColor.RESET);
+                                    Set<String> worldNames = plugin.getConfig().getConfigurationSection("worlds").getKeys(false);
+                                    for (String wname : worldNames) {
+                                        String enabled = plugin.getConfig().getString("worlds." + wname);
+                                        sender.sendMessage("      " + ChatColor.GREEN + wname + ": " + ChatColor.RESET + enabled);
+                                    }
                                 }
-                            }
-                            if (cname.equals("rechargers")) {
-                                sender.sendMessage(ChatColor.AQUA + cname + ":" + ChatColor.RESET);
-                                Set<String> chargerNames = plugin.getConfig().getConfigurationSection("rechargers").getKeys(false);
-                                for (String charname : chargerNames) {
-                                    sender.sendMessage("      " + ChatColor.GREEN + charname + ":");
-                                    sender.sendMessage("            world: " + plugin.getConfig().getString("rechargers." + charname + ".world"));
-                                    sender.sendMessage("            x: " + plugin.getConfig().getString("rechargers." + charname + ".x"));
-                                    sender.sendMessage("            y: " + plugin.getConfig().getString("rechargers." + charname + ".y"));
-                                    sender.sendMessage("            z: " + plugin.getConfig().getString("rechargers." + charname + ".z"));
+                                if (cname.equals("rooms")) {
+                                    sender.sendMessage(ChatColor.AQUA + cname + ":" + ChatColor.RESET);
+                                    Set<String> roomNames = plugin.getConfig().getConfigurationSection("rooms").getKeys(false);
+                                    for (String r : roomNames) {
+                                        sender.sendMessage("      " + ChatColor.GREEN + r + ":");
+                                        sender.sendMessage("            cost: " + plugin.getConfig().getString("rooms." + r + ".cost"));
+                                        sender.sendMessage("            seed: " + plugin.getConfig().getString("rooms." + r + ".seed"));
+                                    }
+                                }
+                                if (cname.equals("rechargers")) {
+                                    sender.sendMessage(ChatColor.AQUA + cname + ":" + ChatColor.RESET);
+                                    Set<String> chargerNames = plugin.getConfig().getConfigurationSection("rechargers").getKeys(false);
+                                    for (String charname : chargerNames) {
+                                        sender.sendMessage("      " + ChatColor.GREEN + charname + ":");
+                                        sender.sendMessage("            world: " + plugin.getConfig().getString("rechargers." + charname + ".world"));
+                                        sender.sendMessage("            x: " + plugin.getConfig().getString("rechargers." + charname + ".x"));
+                                        sender.sendMessage("            y: " + plugin.getConfig().getString("rechargers." + charname + ".y"));
+                                        sender.sendMessage("            z: " + plugin.getConfig().getString("rechargers." + charname + ".z"));
+                                    }
                                 }
                             } else {
                                 sender.sendMessage(ChatColor.AQUA + cname + ": " + ChatColor.RESET + value);
