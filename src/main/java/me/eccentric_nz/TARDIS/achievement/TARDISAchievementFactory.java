82b8adbdd8d55a5a027e87d71cc59c7de189d47e
eccentric devotion <eccentricdevotion@gmail.com>
2013-04-07 01:03:40 +1300

    Add factory to process achievements

---
+++ src/main/java/me/eccentric_nz/TARDIS/achievement/TARDISAchievementFactory.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright (C) 2013 eccentric_nz
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package me.eccentric_nz.TARDIS.achievement;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import me.eccentric_nz.TARDIS.TARDIS;
+import me.eccentric_nz.TARDIS.database.QueryFactory;
+import me.eccentric_nz.TARDIS.database.ResultSetAchievements;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+/**
+ *
+ * @author eccentric_nz
+ */
+public class TARDISAchievementFactory {
+
+    private final TARDIS plugin;
+    private Player player;
+    private String name;
+    int size;
+
+    public TARDISAchievementFactory(TARDIS plugin, Player player, String name, int size) {
+        this.plugin = plugin;
+        this.player = player;
+        this.name = name;
+        this.size = size;
+    }
+
+    public void doAchievement(Object obj) {
+        QueryFactory qf = new QueryFactory(plugin);
+        // have they started the achievement?
+        HashMap<String, Object> wherea = new HashMap<String, Object>();
+        wherea.put("player", player.getName());
+        wherea.put("name", name);
+        wherea.put("completed", 0);
+        ResultSetAchievements rsa = new ResultSetAchievements(plugin, wherea, false);
+        HashMap<String, Object> seta = new HashMap<String, Object>();
+        if (rsa.resultSet()) {
+            HashMap<String, Object> wherem = new HashMap<String, Object>();
+            wherem.put("a_id", rsa.getA_id());
+            boolean achieved = false;
+            // check if the achievement has been reached
+            List<String> data = null;
+            if (obj.getClass().equals(String.class)) {
+                // farm mobs & rooms - have they got this type before?
+                data = Arrays.asList(rsa.getAmount().split(":"));
+                if ((data.size() + 1) == size) {
+                    achieved = true;
+                }
+            } else {
+                int req = plugin.ayml.getInt(name + ".required");
+                int have = plugin.utils.parseNum(rsa.getAmount());
+                int sum = have + (Integer) obj;
+                if (sum >= req) {
+                    achieved = true;
+                }
+            }
+            if (achieved) {
+                // award achievement!
+                int reward_amount = plugin.ayml.getInt(name + ".reward_amount");
+                String reward_type = plugin.ayml.getString(name + ".reward_type");
+                TARDISAchievementNotify tan = new TARDISAchievementNotify(plugin);
+                tan.sendAchievement(player, plugin.ayml.getString(name + ".message"), Material.valueOf(plugin.ayml.getString(name + ".icon")));
+                if (reward_type.equalsIgnoreCase("XP")) {
+                    new TARDISXPRewarder(player).changeExp(reward_amount);
+                } else {
+                    ItemStack is = new ItemStack(Material.valueOf(reward_type), reward_amount);
+                    Inventory inv = player.getInventory();
+                    HashMap<Integer, ItemStack> excess = inv.addItem(is);
+                    for (Map.Entry<Integer, ItemStack> me : excess.entrySet()) {
+                        player.getWorld().dropItem(player.getLocation(), me.getValue());
+                    }
+                }
+                // set achievement as done
+                seta.put("completed", 1);
+            } else {
+                if (obj.getClass().equals(String.class)) {
+                    if (data != null && !data.contains((String) obj)) {
+                        seta.put("amount", rsa.getAmount() + ":" + obj);
+                        qf.doUpdate("achievements", seta, wherem);
+                    }
+                } else {
+                    seta.put("amount", plugin.utils.parseNum(rsa.getAmount()) + (Integer) obj);
+                    qf.doUpdate("achievements", seta, wherem);
+                }
+            }
+        } else {
+            // is it an auto achievement?
+            if (plugin.ayml.getBoolean(name + ".auto")) {
+                // insert a new record
+                seta.put("player", player.getName());
+                seta.put("name", name);
+                seta.put("amount", obj);
+                qf.doInsert("achievements", seta);
+            }
+        }
+    }
+}
--- src/main/java/me/eccentric_nz/TARDIS/travel/TARDISFarmer.java
+++ src/main/java/me/eccentric_nz/TARDIS/travel/TARDISFarmer.java
@@ -21,6 +21,7 @@ import java.util.HashMap;
 import java.util.List;
 import me.eccentric_nz.TARDIS.TARDIS;
 import me.eccentric_nz.TARDIS.TARDISConstants.COMPASS;
+import me.eccentric_nz.TARDIS.achievement.TARDISAchievementFactory;
 import me.eccentric_nz.TARDIS.database.ResultSetTardis;
 import org.bukkit.Location;
 import org.bukkit.Material;
@@ -71,145 +72,167 @@ public class TARDISFarmer {
                 break;
         }
         l.setY(l.getY() + 1);
-        List<Entity> old_macd_had_a_chicken = new ArrayList<Entity>();
-        List<Entity> old_macd_had_a_cow = new ArrayList<Entity>();
-        List<Entity> old_macd_had_a_pig = new ArrayList<Entity>();
-        List<Entity> old_macd_had_a_sheep = new ArrayList<Entity>();
-        List<Entity> old_macd_had_a_mooshroom = new ArrayList<Entity>();
         // spawn an entity at this location so we can get nearby entities - an egg will do
         World w = l.getWorld();
         Entity ent = w.spawnEntity(l, EntityType.EGG);
         List<Entity> mobs = ent.getNearbyEntities(3.5D, 3.5D, 3.5D);
-        for (Entity e : mobs) {
-            switch (e.getType()) {
-                case CHICKEN:
-                    old_macd_had_a_chicken.add(e);
-                    break;
-                case COW:
-                    old_macd_had_a_cow.add(e);
-                    break;
-                case PIG:
-                    old_macd_had_a_pig.add(e);
-                    break;
-                case SHEEP:
-                    old_macd_had_a_sheep.add(e);
-                    break;
-                case MUSHROOM_COW:
-                    old_macd_had_a_mooshroom.add(e);
-                    break;
-                default:
-                    break;
+        if (mobs.size() > 0) {
+            List<Entity> old_macd_had_a_chicken = new ArrayList<Entity>();
+            List<Entity> old_macd_had_a_cow = new ArrayList<Entity>();
+            List<Entity> old_macd_had_a_pig = new ArrayList<Entity>();
+            List<Entity> old_macd_had_a_sheep = new ArrayList<Entity>();
+            List<Entity> old_macd_had_a_mooshroom = new ArrayList<Entity>();
+            // are we doing an achievement?
+            TARDISAchievementFactory taf = null;
+            if (plugin.ayml.getBoolean("farm.enabled")) {
+                taf = new TARDISAchievementFactory(plugin, p, "farm", 5);
+            }
+            for (Entity e : mobs) {
+                switch (e.getType()) {
+                    case CHICKEN:
+                        old_macd_had_a_chicken.add(e);
+                        if (taf != null) {
+                            taf.doAchievement("CHICKEN");
+                        }
+                        break;
+                    case COW:
+                        old_macd_had_a_cow.add(e);
+                        if (taf != null) {
+                            taf.doAchievement("COW");
+                        }
+                        break;
+                    case PIG:
+                        old_macd_had_a_pig.add(e);
+                        if (taf != null) {
+                            taf.doAchievement("PIG");
+                        }
+                        break;
+                    case SHEEP:
+                        old_macd_had_a_sheep.add(e);
+                        if (taf != null) {
+                            taf.doAchievement("SHEEP");
+                        }
+                        break;
+                    case MUSHROOM_COW:
+                        old_macd_had_a_mooshroom.add(e);
+                        if (taf != null) {
+                            taf.doAchievement("MUSHROOM_COW");
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+            // is there a farm room?
+            HashMap<String, Object> where = new HashMap<String, Object>();
+            where.put("tardis_id", id);
+            ResultSetTardis rs = new ResultSetTardis(plugin, where, "", false);
+            if (rs.resultSet()) {
+                String farm = rs.getFarm();
+                if (!farm.equals("")) {
+                    // get location of farm room
+                    String[] data = farm.split(":");
+                    World world = plugin.getServer().getWorld(data[0]);
+                    int x = plugin.utils.parseNum(data[1]);
+                    int y = plugin.utils.parseNum(data[2]) + 1;
+                    int z = plugin.utils.parseNum(data[3]);
+                    if (old_macd_had_a_chicken.size() > 0) {
+                        Location chicken_pen = new Location(world, x + 3, y, z - 3);
+                        while (!world.getChunkAt(chicken_pen).isLoaded()) {
+                            world.getChunkAt(chicken_pen).load();
+                        }
+                        for (Entity e : old_macd_had_a_chicken) {
+                            plugin.myspawn = true;
+                            world.spawnEntity(chicken_pen, EntityType.CHICKEN);
+                            e.remove();
+                        }
+                    }
+                    if (old_macd_had_a_cow.size() > 0) {
+                        Location cow_pen = new Location(world, x + 3, y, z + 3);
+                        while (!world.getChunkAt(cow_pen).isLoaded()) {
+                            world.getChunkAt(cow_pen).load();
+                        }
+                        for (Entity e : old_macd_had_a_cow) {
+                            plugin.myspawn = true;
+                            world.spawnEntity(cow_pen, EntityType.COW);
+                            e.remove();
+                        }
+                    }
+                    if (old_macd_had_a_pig.size() > 0) {
+                        Location pig_pen = new Location(world, x - 3, y, z - 3);
+                        while (!world.getChunkAt(pig_pen).isLoaded()) {
+                            world.getChunkAt(pig_pen).load();
+                        }
+                        for (Entity e : old_macd_had_a_pig) {
+                            plugin.myspawn = true;
+                            world.spawnEntity(pig_pen, EntityType.PIG);
+                            e.remove();
+                        }
+                    }
+                    if (old_macd_had_a_sheep.size() > 0) {
+                        Location sheep_pen = new Location(world, x - 3, y, z + 3);
+                        while (!world.getChunkAt(sheep_pen).isLoaded()) {
+                            world.getChunkAt(sheep_pen).load();
+                        }
+                        for (Entity e : old_macd_had_a_sheep) {
+                            plugin.myspawn = true;
+                            world.spawnEntity(sheep_pen, EntityType.SHEEP);
+                            e.remove();
+                        }
+                    }
+                    if (old_macd_had_a_mooshroom.size() > 0) {
+                        Location cow_pen = new Location(world, x + 3, y, z + 3);
+                        while (!world.getChunkAt(cow_pen).isLoaded()) {
+                            world.getChunkAt(cow_pen).load();
+                        }
+                        for (Entity e : old_macd_had_a_mooshroom) {
+                            plugin.myspawn = true;
+                            world.spawnEntity(cow_pen, EntityType.MUSHROOM_COW);
+                            e.remove();
+                        }
+                    }
+                } else {
+                    // no farm, give the player spawn eggs
+                    Inventory inv = p.getInventory();
+                    if (old_macd_had_a_chicken.size() > 0) {
+                        for (Entity e : old_macd_had_a_chicken) {
+                            ItemStack is = new ItemStack(Material.MONSTER_EGG, 1, (short) 93);
+                            inv.addItem(is);
+                            e.remove();
+                        }
+                    }
+                    if (old_macd_had_a_cow.size() > 0) {
+                        for (Entity e : old_macd_had_a_cow) {
+                            ItemStack is = new ItemStack(Material.MONSTER_EGG, 1, (short) 92);
+                            inv.addItem(is);
+                            e.remove();
+                        }
+                    }
+                    if (old_macd_had_a_pig.size() > 0) {
+                        for (Entity e : old_macd_had_a_pig) {
+                            ItemStack is = new ItemStack(Material.MONSTER_EGG, 1, (short) 90);
+                            inv.addItem(is);
+                            e.remove();
+                        }
+                    }
+                    if (old_macd_had_a_sheep.size() > 0) {
+                        for (Entity e : old_macd_had_a_sheep) {
+                            ItemStack is = new ItemStack(Material.MONSTER_EGG, 1, (short) 91);
+                            inv.addItem(is);
+                            e.remove();
+                        }
+                    }
+                    if (old_macd_had_a_mooshroom.size() > 0) {
+                        for (Entity e : old_macd_had_a_mooshroom) {
+                            ItemStack is = new ItemStack(Material.MONSTER_EGG, 1, (short) 96);
+                            inv.addItem(is);
+                            e.remove();
+                        }
+                    }
+                    p.updateInventory();
+                }
             }
         }
         ent.remove();
-        // is there a farm room?
-        HashMap<String, Object> where = new HashMap<String, Object>();
-        where.put("tardis_id", id);
-        ResultSetTardis rs = new ResultSetTardis(plugin, where, "", false);
-        if (rs.resultSet()) {
-            String farm = rs.getFarm();
-            if (!farm.equals("")) {
-                // get location of farm room
-                String[] data = farm.split(":");
-                World world = plugin.getServer().getWorld(data[0]);
-                int x = plugin.utils.parseNum(data[1]);
-                int y = plugin.utils.parseNum(data[2]) + 1;
-                int z = plugin.utils.parseNum(data[3]);
-                if (old_macd_had_a_chicken.size() > 0) {
-                    Location chicken_pen = new Location(world, x + 3, y, z - 3);
-                    while (!world.getChunkAt(chicken_pen).isLoaded()) {
-                        world.getChunkAt(chicken_pen).load();
-                    }
-                    for (Entity e : old_macd_had_a_chicken) {
-                        plugin.myspawn = true;
-                        world.spawnEntity(chicken_pen, EntityType.CHICKEN);
-                        e.remove();
-                    }
-                }
-                if (old_macd_had_a_cow.size() > 0) {
-                    Location cow_pen = new Location(world, x + 3, y, z + 3);
-                    while (!world.getChunkAt(cow_pen).isLoaded()) {
-                        world.getChunkAt(cow_pen).load();
-                    }
-                    for (Entity e : old_macd_had_a_cow) {
-                        plugin.myspawn = true;
-                        world.spawnEntity(cow_pen, EntityType.COW);
-                        e.remove();
-                    }
-                }
-                if (old_macd_had_a_pig.size() > 0) {
-                    Location pig_pen = new Location(world, x - 3, y, z - 3);
-                    while (!world.getChunkAt(pig_pen).isLoaded()) {
-                        world.getChunkAt(pig_pen).load();
-                    }
-                    for (Entity e : old_macd_had_a_pig) {
-                        plugin.myspawn = true;
-                        world.spawnEntity(pig_pen, EntityType.PIG);
-                        e.remove();
-                    }
-                }
-                if (old_macd_had_a_sheep.size() > 0) {
-                    Location sheep_pen = new Location(world, x - 3, y, z + 3);
-                    while (!world.getChunkAt(sheep_pen).isLoaded()) {
-                        world.getChunkAt(sheep_pen).load();
-                    }
-                    for (Entity e : old_macd_had_a_sheep) {
-                        plugin.myspawn = true;
-                        world.spawnEntity(sheep_pen, EntityType.SHEEP);
-                        e.remove();
-                    }
-                }
-                if (old_macd_had_a_mooshroom.size() > 0) {
-                    Location cow_pen = new Location(world, x + 3, y, z + 3);
-                    while (!world.getChunkAt(cow_pen).isLoaded()) {
-                        world.getChunkAt(cow_pen).load();
-                    }
-                    for (Entity e : old_macd_had_a_mooshroom) {
-                        plugin.myspawn = true;
-                        world.spawnEntity(cow_pen, EntityType.MUSHROOM_COW);
-                        e.remove();
-                    }
-                }
-            } else {
-                // no farm, give the player spawn eggs
-                Inventory inv = p.getInventory();
-                if (old_macd_had_a_chicken.size() > 0) {
-                    for (Entity e : old_macd_had_a_chicken) {
-                        ItemStack is = new ItemStack(Material.MONSTER_EGG, 1, (short) 93);
-                        inv.addItem(is);
-                        e.remove();
-                    }
-                }
-                if (old_macd_had_a_cow.size() > 0) {
-                    for (Entity e : old_macd_had_a_cow) {
-                        ItemStack is = new ItemStack(Material.MONSTER_EGG, 1, (short) 92);
-                        inv.addItem(is);
-                        e.remove();
-                    }
-                }
-                if (old_macd_had_a_pig.size() > 0) {
-                    for (Entity e : old_macd_had_a_pig) {
-                        ItemStack is = new ItemStack(Material.MONSTER_EGG, 1, (short) 90);
-                        inv.addItem(is);
-                        e.remove();
-                    }
-                }
-                if (old_macd_had_a_sheep.size() > 0) {
-                    for (Entity e : old_macd_had_a_sheep) {
-                        ItemStack is = new ItemStack(Material.MONSTER_EGG, 1, (short) 91);
-                        inv.addItem(is);
-                        e.remove();
-                    }
-                }
-                if (old_macd_had_a_mooshroom.size() > 0) {
-                    for (Entity e : old_macd_had_a_mooshroom) {
-                        ItemStack is = new ItemStack(Material.MONSTER_EGG, 1, (short) 96);
-                        inv.addItem(is);
-                        e.remove();
-                    }
-                }
-                p.updateInventory();
-            }
-        }
     }
 }
