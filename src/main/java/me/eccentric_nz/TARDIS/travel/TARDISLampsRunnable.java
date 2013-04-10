e6c78162d83760ecdc84a4ce721753c3973e7f43
eccentric devotion <eccentricdevotion@gmail.com>
2013-04-10 20:42:51 +1200

    Add runnable to flicker the lamps on and off

---
+++ src/main/java/me/eccentric_nz/TARDIS/travel/TARDISLampsRunnable.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2013 eccentric_nz
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package me.eccentric_nz.TARDIS.travel;
+
+import java.util.List;
+import me.eccentric_nz.TARDIS.TARDIS;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+
+/**
+ * Phosphor lamps are used for lighting. They use electron excitation; when
+ * shaken, they grew brighter.
+ *
+ * @author eccentric_nz
+ */
+public class TARDISLampsRunnable implements Runnable {
+
+    private final TARDIS plugin;
+    private final List<Block> lamps;
+    private final long start;
+    private int task;
+
+    public TARDISLampsRunnable(TARDIS plugin, List<Block> lamps, long start) {
+        this.plugin = plugin;
+        this.lamps = lamps;
+        this.start = start;
+    }
+
+    @Override
+    public void run() {
+        for (Block b : lamps) {
+            if (b.getType() == Material.REDSTONE_LAMP_ON) {
+                b.setType(Material.REDSTONE_LAMP_OFF);
+            } else {
+                b.setType(Material.REDSTONE_LAMP_ON);
+            }
+        }
+        if (System.currentTimeMillis() > start) {
+            // set all lamps back to on
+            for (Block b : lamps) {
+                b.setType(Material.REDSTONE_LAMP_ON);
+            }
+            plugin.getServer().getScheduler().cancelTask(task);
+        }
+    }
+
+    public void setTask(int task) {
+        this.task = task;
+    }
+}
--- src/main/java/me/eccentric_nz/TARDIS/travel/TARDISMalfunction.java
+++ src/main/java/me/eccentric_nz/TARDIS/travel/TARDISMalfunction.java
@@ -16,15 +16,17 @@
  */
 package me.eccentric_nz.TARDIS.travel;
 
-import java.lang.String;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Random;
 import me.eccentric_nz.TARDIS.TARDIS;
-import me.eccentric_nz.TARDIS.database.ResultSetChunks;
+import me.eccentric_nz.TARDIS.TARDISConstants;
+import me.eccentric_nz.TARDIS.database.ResultSetLamps;
 import org.bukkit.Effect;
 import org.bukkit.Location;
 import org.bukkit.Sound;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.getspout.spoutapi.SpoutManager;
 
@@ -37,37 +39,44 @@ public class TARDISMalfunction {
     private final TARDIS plugin;
     private int id;
     private Player p;
+    private TARDISConstants.COMPASS dir;
     private Random rand;
 
-    public TARDISMalfunction(TARDIS plugin, int id, Player p) {
+    public TARDISMalfunction(TARDIS plugin, int id, Player p, TARDISConstants.COMPASS dir) {
         this.plugin = plugin;
         this.id = id;
         this.p = p;
+        this.dir = dir;
         this.rand = new Random();
     }
 
-    public Location isMalfunction() {
-        Location mal = null;
+    public boolean isMalfunction() {
+        boolean mal = false;
         if (plugin.getConfig().getInt("malfunction") > 0) {
             int chance = 100 - plugin.getConfig().getInt("malfunction");
             if (rand.nextInt(100) > chance) {
-                mal = getMalfunction();
+                mal = true;
             }
         }
         return mal;
     }
 
     public Location getMalfunction() {
-        Location l = null;
+        Location l;
         int end = 100 - plugin.getConfig().getInt("malfunction_end");
         int nether = end - plugin.getConfig().getInt("malfunction_nether");
         int r = rand.nextInt(100);
+        TARDISTimetravel tt = new TARDISTimetravel(plugin);
+        byte x = (byte) 2;
         if (r > end) {
             // get random the_end location
+            l = tt.randomDestination(p, x, x, x, dir, "THE_END");
         } else if (r > nether) {
             // get random nether location
+            l = tt.randomDestination(p, x, x, x, dir, "NETHER");
         } else {
             // get random normal location
+            l = tt.randomDestination(p, x, x, x, dir, "NORMAL");
         }
         if (l != null) {
             doMalfunction(l);
@@ -78,12 +87,19 @@ public class TARDISMalfunction {
     public void doMalfunction(Location l) {
         HashMap<String, Object> where = new HashMap<String, Object>();
         where.put("tardis_id", id);
-        ResultSetChunks rsc = new ResultSetChunks(plugin, where, true);
-        if (rsc.resultSet()) {
+        ResultSetLamps rsl = new ResultSetLamps(plugin, where, true);
+        List<Block> lamps = new ArrayList<Block>();
+        if (rsl.resultSet()) {
             // flicker lights
-            ArrayList<HashMap<String, String>> data = rsc.getData();
+            ArrayList<HashMap<String, String>> data = rsl.getData();
             for (HashMap<String, String> map : data) {
+                Location loc = plugin.utils.getLocationFromDB(map.get("location"), 0.0F, 0.0F);
+                lamps.add(loc.getBlock());
             }
+            final long start = System.currentTimeMillis() + 5000;
+            TARDISLampsRunnable runnable = new TARDISLampsRunnable(plugin, lamps, start);
+            int taskID = plugin.getServer().getScheduler().scheduleSyncRepeatingTask(plugin, runnable, 1L, 5L);
+            runnable.setTask(taskID);
             // play tardis crash sound
             if (plugin.pm.getPlugin("Spout") != null && SpoutManager.getPlayer(p).isSpoutCraftEnabled()) {
                 SpoutManager.getSoundManager().playGlobalCustomSoundEffect(plugin, "https://dl.dropbox.com/u/53758864/tardis_emergency_land.mp3", false, l, 20, 75);
@@ -95,7 +111,6 @@ public class TARDISMalfunction {
                     l.getWorld().playEffect(l, Effect.BLAZE_SHOOT, 0);
                 }
             }
-            // emergency set handbrake
         }
     }
 }
