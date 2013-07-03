d50ec047fc982ca51a82fa6352bc22f7f22d369b
eccentric devotion <eccentricdevotion@gmail.com>
2013-07-03 22:39:44 +1200

    Bukkit can't yet detect horses :(

--- src/main/java/me/eccentric_nz/TARDIS/travel/TARDISFarmer.java
+++ src/main/java/me/eccentric_nz/TARDIS/travel/TARDISFarmer.java
@@ -111,6 +111,7 @@ public class TARDISFarmer {
             // count total mobs
             int total = 0;
             for (Entity e : mobs) {
+                plugin.debug(e.getType());
                 switch (e.getType()) {
                     case CHICKEN:
                         TARDISMob tmchk = new TARDISMob();
