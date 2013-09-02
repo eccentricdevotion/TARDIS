af678d39fb3f403539950f7cbe16d088ff9e8786
eccentric devotion <eccentricdevotion@gmail.com>
2013-09-02 22:32:43 +1200

    Add empty config section for `no_artron_value`

--- src/main/java/me/eccentric_nz/TARDIS/files/TARDISConfiguration.java
+++ src/main/java/me/eccentric_nz/TARDIS/files/TARDISConfiguration.java
@@ -18,6 +18,7 @@ package me.eccentric_nz.TARDIS.files;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -431,6 +432,10 @@ public class TARDISConfiguration {
         if (!blocks_config.contains("under_door_blocks")) {
             List<Integer> UNDER_BLOCKS = Arrays.asList(new Integer[]{0, 6, 8, 9, 10, 11, 18, 20, 26, 27, 28, 29, 30, 31, 32, 33, 34, 37, 38, 39, 40, 44, 46, 50, 51, 53, 54, 55, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 75, 76, 77, 78, 79, 81, 83, 85, 89, 92, 93, 94, 96, 101, 102, 104, 105, 106, 107, 108, 109, 111, 113, 114, 115, 116, 117, 118, 119, 120, 122, 126, 128, 130, 131, 132, 134, 135, 136});
             blocks_config.set("under_door_blocks", UNDER_BLOCKS);
+            i++;
+        }
+        if (!blocks_config.contains("no_artron_value")) {
+            blocks_config.set("no_artron_value", new ArrayList<String>());
             i++;
         }
         try {
