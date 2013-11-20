2480705cae03e39f041df2702e41fed8b80ce8ec
eccentric devotion <eccentricdevotion@gmail.com>
2013-11-20 22:16:30 +1300

    Update credits

--- src/main/java/me/eccentric_nz/TARDIS/builders/TARDISBuilderInner.java
+++ src/main/java/me/eccentric_nz/TARDIS/builders/TARDISBuilderInner.java
@@ -80,7 +80,7 @@ public class TARDISBuilderInner {
         boolean below = (!plugin.getConfig().getBoolean("create_worlds") && !plugin.getConfig().getBoolean("default_world"));
         int starty = (below) ? 15 : 64;
         switch (schm) {
-            // some TARDIS schematics supplied by ewized http://dev.bukkit.org/profiles/ewized/
+            // TARDIS schematics supplied by Lord_Rahl and the team at mcnovus.net
             case BIGGER:
                 s = plugin.biggerschematic;
                 d = plugin.biggerdimensions;
