312199f501fad6cf2c50727b6ba903c451d5798f
eccentric devotion <eccentricdevotion@gmail.com>
2013-11-29 21:15:41 +1300

    Move data recalculators into their own classes

---
+++ src/main/java/me/eccentric_nz/TARDIS/utility/recalculators/TARDISButtonRecalculator.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright (C) 2013 eccentric_nz
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package me.eccentric_nz.TARDIS.utility.recalculators;
+
+import me.eccentric_nz.TARDIS.TARDISConstants;
+
+/**
+ *
+ * @author eccentric_nz
+ */
+public class TARDISButtonRecalculator {
+
+    /**
+     * Recalculate the data for directional block (TRAPDOOR) when the TARDIS
+     * preset changes direction.
+     *
+     * @param b the byte stored in the preset data
+     * @param d the new direction of the TARDIS
+     * @return the recalculated byte
+     */
+    public byte recalculate(byte b, TARDISConstants.COMPASS d) {
+        byte recalc;
+        switch (d) {
+            case SOUTH:
+                switch (b) {
+                    case 1:
+                        recalc = 3;
+                        break;
+                    case 2:
+                        recalc = 4;
+                        break;
+                    case 3:
+                        recalc = 2;
+                        break;
+                    case 4:
+                        recalc = 1;
+                        break;
+                    default:
+                        recalc = b;
+                }
+                break;
+            case WEST:
+                switch (b) {
+                    case 1:
+                        recalc = 2;
+                        break;
+                    case 2:
+                        recalc = 1;
+                        break;
+                    case 3:
+                        recalc = 4;
+                        break;
+                    case 4:
+                        recalc = 3;
+                        break;
+                    default:
+                        recalc = b;
+                }
+                break;
+            default:
+                switch (b) {
+                    case 1:
+                        recalc = 4;
+                        break;
+                    case 2:
+                        recalc = 3;
+                        break;
+                    case 3:
+                        recalc = 1;
+                        break;
+                    case 4:
+                        recalc = 2;
+                        break;
+                    default:
+                        recalc = b;
+                }
+                break;
+        }
+        return recalc;
+    }
+
+}
