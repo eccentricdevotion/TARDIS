cdca59f23803b9494dc01c86c2f06ff5a0059487
eccentric devotion <eccentricdevotion@gmail.com>
2014-01-20 20:19:35 +1300

    Add the ability for the Sonic Screwdriver to sort chests - permission `tardis.sonic.sort`
    Code borrowed for the most part from
    http://dev.bukkit.org/bukkit-plugins/simplesort/ MIT License included
    in source.

--- src/main/java/me/eccentric_nz/TARDIS/sonic/TARDISSonicListener.java
+++ src/main/java/me/eccentric_nz/TARDIS/sonic/TARDISSonicListener.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-package me.eccentric_nz.TARDIS.listeners;
+package me.eccentric_nz.TARDIS.sonic;
 
 import com.griefcraft.lwc.LWC;
 import java.util.ArrayList;
