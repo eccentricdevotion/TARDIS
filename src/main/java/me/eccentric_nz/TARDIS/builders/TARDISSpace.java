f4b254dd9169e47c2ac2664a8731584f59a92c53
eccentric devotion <eccentricdevotion@gmail.com>
2014-03-20 21:04:38 +1300

    If using Multiverse, don't set `animals false`

--- src/main/java/me/eccentric_nz/TARDIS/builders/TARDISSpace.java
+++ src/main/java/me/eccentric_nz/TARDIS/builders/TARDISSpace.java
@@ -76,7 +76,7 @@ public class TARDISSpace {
             plugin.saveConfig();
             if (plugin.getPM().isPluginEnabled("Multiverse-Core")) {
                 plugin.getServer().dispatchCommand(plugin.getConsole(), "mv import " + name + " normal -g TARDISChunkGenerator -n");
-                plugin.getServer().dispatchCommand(plugin.getConsole(), "mv modify set animals false " + name);
+//                plugin.getServer().dispatchCommand(plugin.getConsole(), "mv modify set animals false " + name);
                 plugin.getServer().dispatchCommand(plugin.getConsole(), "mv modify set monsters false " + name);
                 plugin.getServer().dispatchCommand(plugin.getConsole(), "mv modify set hidden true " + name);
                 plugin.getServer().dispatchCommand(plugin.getConsole(), "mv modify set weather false " + name);
@@ -170,7 +170,7 @@ public class TARDISSpace {
         plugin.saveConfig();
         if (plugin.getPM().isPluginEnabled("Multiverse-Core")) {
             plugin.getServer().dispatchCommand(plugin.getConsole(), "mv import " + name + " normal -g TARDISChunkGenerator -n");
-            plugin.getServer().dispatchCommand(plugin.getConsole(), "mv modify set animals false " + name);
+//            plugin.getServer().dispatchCommand(plugin.getConsole(), "mv modify set animals false " + name);
             plugin.getServer().dispatchCommand(plugin.getConsole(), "mv modify set monsters false " + name);
             plugin.getServer().dispatchCommand(plugin.getConsole(), "mv modify set hidden true " + name);
             plugin.getServer().dispatchCommand(plugin.getConsole(), "mv modify set weather false " + name);
