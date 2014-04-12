75389c67e35bc6a63bd93a503e6556b08a2168d0
eccentric devotion <eccentricdevotion@gmail.com>
2014-04-12 19:58:22 +1200

    Bump minimum CraftBukkit version to 1.7.8

--- src/main/java/me/eccentric_nz/TARDIS/TARDIS.java
+++ src/main/java/me/eccentric_nz/TARDIS/TARDIS.java
@@ -145,7 +145,7 @@ public class TARDIS extends JavaPlugin {
         plugin = this;
         console = getServer().getConsoleSender();
         Version bukkitversion = getServerVersion(getServer().getVersion());
-        Version minversion = new Version("1.7.5");
+        Version minversion = new Version("1.7.8");
         // check CraftBukkit version
         if (bukkitversion.compareTo(minversion) >= 0) {
             hasVersion = true;
