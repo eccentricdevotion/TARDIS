1fff1ce2f2beb25166721bdd66f96b3ed08bc34c
eccentric devotion <eccentricdevotion@gmail.com>
2014-05-06 19:45:04 +1200

    Always remove the trackers even if there is no outer door

--- src/main/java/me/eccentric_nz/TARDIS/move/TARDISDoorCloser.java
+++ src/main/java/me/eccentric_nz/TARDIS/move/TARDISDoorCloser.java
@@ -25,7 +25,6 @@ import me.eccentric_nz.TARDIS.database.ResultSetCurrentLocation;
 import me.eccentric_nz.TARDIS.database.ResultSetDoorBlocks;
 import me.eccentric_nz.TARDIS.database.ResultSetTardis;
 import me.eccentric_nz.TARDIS.enumeration.COMPASS;
-import me.eccentric_nz.TARDIS.travel.TARDISDoorLocation;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
@@ -48,10 +47,9 @@ public class TARDISDoorCloser {
 
     public void closeDoors() {
         // get door locations
-        // outer
         final ResultSetDoorBlocks rs = new ResultSetDoorBlocks(plugin, id);
         if (rs.resultSet()) {
-            close(rs.getOuterBlock(), true, rs.getOuterDirection());
+            close(rs.getOuterBlock(), rs.getInnerBlock().getLocation(), rs.getOuterDirection());
             // inner
             if (!rs.getInnerBlock().getChunk().isLoaded()) {
                 rs.getInnerBlock().getChunk().load();
@@ -59,7 +57,7 @@ public class TARDISDoorCloser {
             plugin.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
                 @Override
                 public void run() {
-                    close(rs.getInnerBlock(), false, rs.getInnerDirection());
+                    close(rs.getInnerBlock(), null, rs.getInnerDirection());
                 }
             }, 5L);
         }
@@ -73,7 +71,7 @@ public class TARDISDoorCloser {
      * @param dd the door direction
      */
     @SuppressWarnings("deprecation")
-    private void close(Block block, boolean remove, COMPASS dd) {
+    private void close(Block block, Location inportal, COMPASS dd) {
         if (block.getType().equals(Material.IRON_DOOR_BLOCK) || block.getType().equals(Material.WOODEN_DOOR)) {
             byte door_data = block.getData();
             switch (dd) {
@@ -98,42 +96,39 @@ public class TARDISDoorCloser {
                     }
                     break;
             }
-            if (remove && plugin.getConfig().getBoolean("preferences.walk_in_tardis")) {
-                // get all companion UUIDs
-                List<UUID> uuids = new ArrayList<UUID>();
-                uuids.add(uuid);
-                HashMap<String, Object> where = new HashMap<String, Object>();
-                where.put("tardis_id", id);
-                ResultSetTardis rs = new ResultSetTardis(plugin, where, "", false);
-                if (rs.resultSet()) {
-                    String[] companions = rs.getCompanions().split(":");
-                    for (String c : companions) {
-                        if (!c.isEmpty()) {
-                            uuids.add(UUID.fromString(c));
-                        }
+        }
+        if (inportal != null && plugin.getConfig().getBoolean("preferences.walk_in_tardis")) {
+            // get all companion UUIDs
+            List<UUID> uuids = new ArrayList<UUID>();
+            uuids.add(uuid);
+            HashMap<String, Object> where = new HashMap<String, Object>();
+            where.put("tardis_id", id);
+            ResultSetTardis rs = new ResultSetTardis(plugin, where, "", false);
+            if (rs.resultSet()) {
+                String[] companions = rs.getCompanions().split(":");
+                for (String c : companions) {
+                    if (!c.isEmpty()) {
+                        uuids.add(UUID.fromString(c));
                     }
                 }
-                // get locations
-                // exterior portal (from current location)
-                HashMap<String, Object> where_exportal = new HashMap<String, Object>();
-                where_exportal.put("tardis_id", id);
-                ResultSetCurrentLocation rsc = new ResultSetCurrentLocation(plugin, where_exportal);
-                rsc.resultSet();
-                Location exportal = new Location(rsc.getWorld(), rsc.getX(), rsc.getY(), rsc.getZ());
-                // interior portal
-                TARDISDoorLocation idl = plugin.getGeneralKeeper().getDoorListener().getDoor(1, id);
-                Location inportal = idl.getL();
-                // unset trackers
-                // players
-                for (UUID u : uuids) {
-                    if (plugin.getTrackerKeeper().getTrackMover().contains(u)) {
-                        plugin.getTrackerKeeper().getTrackMover().remove(u);
-                    }
-                }
-                // locations
-                plugin.getTrackerKeeper().getTrackPortals().remove(exportal);
-                plugin.getTrackerKeeper().getTrackPortals().remove(inportal);
             }
+            // get locations
+            // exterior portal (from current location)
+            HashMap<String, Object> where_exportal = new HashMap<String, Object>();
+            where_exportal.put("tardis_id", id);
+            ResultSetCurrentLocation rsc = new ResultSetCurrentLocation(plugin, where_exportal);
+            rsc.resultSet();
+            Location exportal = new Location(rsc.getWorld(), rsc.getX(), rsc.getY(), rsc.getZ());
+            // unset trackers
+            // players
+            for (UUID u : uuids) {
+                if (plugin.getTrackerKeeper().getTrackMover().contains(u)) {
+                    plugin.getTrackerKeeper().getTrackMover().remove(u);
+                }
+            }
+            // locations
+            plugin.getTrackerKeeper().getTrackPortals().remove(exportal);
+            plugin.getTrackerKeeper().getTrackPortals().remove(inportal);
         }
     }
 }
