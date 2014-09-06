081b23a1ff614df730074368ed56c2198ee206d3
eccentric devotion <eccentricdevotion@gmail.com>
2014-09-06 20:00:43 +1200

    Set the EP1 message to the contents of the book in hand

--- src/main/java/me/eccentric_nz/TARDIS/commands/preferences/TARDISEPSMessageCommand.java
+++ src/main/java/me/eccentric_nz/TARDIS/commands/preferences/TARDISEPSMessageCommand.java
@@ -17,10 +17,14 @@
 package me.eccentric_nz.TARDIS.commands.preferences;
 
 import java.util.HashMap;
+import java.util.List;
 import me.eccentric_nz.TARDIS.TARDIS;
 import me.eccentric_nz.TARDIS.database.QueryFactory;
 import me.eccentric_nz.TARDIS.utility.TARDISMessage;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.BookMeta;
 
 /**
  *
@@ -35,17 +39,30 @@ public class TARDISEPSMessageCommand {
     }
 
     public boolean setMessage(Player player, String[] args, QueryFactory qf) {
+        String message;
         int count = args.length;
-        if (count < 2) {
-            TARDISMessage.send(player, "EP1_NEED");
-            return false;
+        ItemStack bq = player.getItemInHand();
+        if (bq != null && (bq.getType().equals(Material.BOOK_AND_QUILL) || bq.getType().equals(Material.WRITTEN_BOOK))) {
+            BookMeta bm = (BookMeta) bq.getItemMeta();
+            List<String> pages = bm.getPages();
+            StringBuilder sb = new StringBuilder();
+            for (String s : pages) {
+                sb.append(s).append(" ");
+            }
+            message = sb.toString();
+            plugin.debug(message);
+        } else {
+            if (count < 2) {
+                TARDISMessage.send(player, "EP1_NEED");
+                return false;
+            }
+            StringBuilder buf = new StringBuilder();
+            for (int i = 1; i < count; i++) {
+                buf.append(args[i]).append(" ");
+            }
+            String tmp = buf.toString();
+            message = tmp.substring(0, tmp.length() - 1);
         }
-        StringBuilder buf = new StringBuilder();
-        for (int i = 1; i < count; i++) {
-            buf.append(args[i]).append(" ");
-        }
-        String tmp = buf.toString();
-        String message = tmp.substring(0, tmp.length() - 1);
         HashMap<String, Object> sete = new HashMap<String, Object>();
         sete.put("eps_message", message);
         HashMap<String, Object> where = new HashMap<String, Object>();
