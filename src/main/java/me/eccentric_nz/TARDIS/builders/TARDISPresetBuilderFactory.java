fdafd298f92dec40e7b6cf94a3c681b93c06514d
eccentricdevotion <eccentricdevotion@gmail.com>
2014-12-22 19:34:14 +1300

    Prevent Police Box duplication after rebuilding (again)

--- src/main/java/me/eccentric_nz/TARDIS/builders/TARDISPresetBuilderFactory.java
+++ src/main/java/me/eccentric_nz/TARDIS/builders/TARDISPresetBuilderFactory.java
@@ -150,13 +150,6 @@ public class TARDISPresetBuilderFactory {
                     TARDISDeinstaPreset deinsta = new TARDISDeinstaPreset(plugin);
                     deinsta.instaDestroyPreset(pbd, false, demat);
                 }
-                final TARDISInstaPreset trp = new TARDISInstaPreset(plugin, pbd, preset, lamp, cham_id, cham_data, true, minecart, ctm, add_sign);
-                plugin.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
-                    @Override
-                    public void run() {
-                        trp.buildPreset();
-                    }
-                }, 10L);
                 plugin.getTrackerKeeper().getMaterialising().add(pbd.getTardisID());
                 TARDISMaterialisationPreset runnable = new TARDISMaterialisationPreset(plugin, pbd, preset, lamp, cham_id, cham_data, minecart, ctm, add_sign, 3);
                 int taskID = plugin.getServer().getScheduler().scheduleSyncRepeatingTask(plugin, runnable, 10L, 20L);
