ef076801675d1b8072b558f2054bdf8efb6e7e85
eccentricdevotion <eccentricdevotion@gmail.com>
2015-09-23 08:59:56 +1200

    Flash the Resdtone Torch when flying the Junk TARDIS

--- src/main/java/me/eccentric_nz/TARDIS/junk/TARDISJunkItsDangerousRunnable.java
+++ src/main/java/me/eccentric_nz/TARDIS/junk/TARDISJunkItsDangerousRunnable.java
@@ -8,6 +8,8 @@ import java.util.List;
 import java.util.UUID;
 import me.eccentric_nz.TARDIS.TARDIS;
 import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 
 /**
@@ -18,14 +20,17 @@ public class TARDISJunkItsDangerousRunnable implements Runnable {
 
     private final TARDIS plugin;
     private final Location l;
+    private final Block t;
     int minX;
     int minZ;
     int maxX;
     int maxZ;
+    int c = 0;
 
     public TARDISJunkItsDangerousRunnable(TARDIS plugin, Location l) {
         this.plugin = plugin;
         this.l = l;
+        this.t = l.clone().add(0.0d, 2.0d, -1.0d).getBlock();
         minX = this.l.getBlockX() - 2;
         minZ = this.l.getBlockZ() - 2;
         maxX = this.l.getBlockX() + 3;
@@ -34,6 +39,9 @@ public class TARDISJunkItsDangerousRunnable implements Runnable {
 
     @Override
     public void run() {
+        if (c % 5 < 3) {
+            t.setType(Material.REDSTONE_TORCH_OFF);
+        }
         // check if player is in Junk TARDIS effects zone
         List<UUID> remove = new ArrayList<UUID>();
         for (UUID uuid : plugin.getGeneralKeeper().getJunkTravellers()) {
@@ -46,6 +54,7 @@ public class TARDISJunkItsDangerousRunnable implements Runnable {
         if (remove.size() > 0) {
             plugin.getGeneralKeeper().getJunkTravellers().removeAll(remove);
         }
+        c++;
     }
 
     public boolean isInside(Location loc) {
