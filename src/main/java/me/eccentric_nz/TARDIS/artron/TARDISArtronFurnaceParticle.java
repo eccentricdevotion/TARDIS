18cf5378f4a6a8fcf5ecc347fb8e54fd55898933
eccentricdevotion <eccentricdevotion@gmail.com>
2015-09-26 12:55:59 +1200

    Add more null checks to Artron Furnace checking

--- src/main/java/me/eccentric_nz/TARDIS/artron/TARDISArtronFurnaceParticle.java
+++ src/main/java/me/eccentric_nz/TARDIS/artron/TARDISArtronFurnaceParticle.java
@@ -7,6 +7,7 @@ import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.Furnace;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
 
 public class TARDISArtronFurnaceParticle {
 
@@ -51,6 +52,13 @@ public class TARDISArtronFurnaceParticle {
             return false;
         }
         Furnace furnace = (Furnace) b.getState();
-        return (furnace != null && furnace.getInventory().getTitle().equals("TARDIS Artron Furnace"));
+        if (furnace != null) {
+            Inventory inv = furnace.getInventory();
+            if (inv != null) {
+                String title = furnace.getInventory().getTitle();
+                return (title != null && title.equals("TARDIS Artron Furnace"));
+            }
+        }
+        return false;
     }
 }
