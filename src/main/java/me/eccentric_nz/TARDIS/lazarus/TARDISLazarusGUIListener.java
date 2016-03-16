6930d770207acb3ae5126f8d0984170162751765
eccentricdevotion <eccentricdevotion@gmail.com>
2016-03-16 18:01:59 +1300

    Just so it builds

--- src/main/java/me/eccentric_nz/TARDIS/lazarus/TARDISLazarusGUIListener.java
+++ src/main/java/me/eccentric_nz/TARDIS/lazarus/TARDISLazarusGUIListener.java
@@ -319,7 +319,8 @@ public class TARDISLazarusGUIListener extends TARDISMenuListener implements List
                                             break;
                                         case BAT:
                                             BatWatcher bw = (BatWatcher) livingWatcher;
-                                            bw.setHanging(!getBoolean(inv));
+                                            //bw.setHanging(!getBoolean(inv));
+                                            bw.setFlying(getBoolean(inv));
                                             break;
                                         case BLAZE:
                                             BlazeWatcher bbw = (BlazeWatcher) livingWatcher;
