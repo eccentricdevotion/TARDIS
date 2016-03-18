e70e86993f4d553161776f442f893622d69dbdc9
eccentricdevotion <eccentricdevotion@gmail.com>
2016-03-18 18:38:43 +1300

    Check the player is in their own TARDIS when using the `/tardis upgrade` command

--- src/main/java/me/eccentric_nz/TARDIS/commands/tardis/TARDISUpgradeCommand.java
+++ src/main/java/me/eccentric_nz/TARDIS/commands/tardis/TARDISUpgradeCommand.java
@@ -18,11 +18,14 @@ package me.eccentric_nz.TARDIS.commands.tardis;
 
 import java.util.HashMap;
 import me.eccentric_nz.TARDIS.TARDIS;
+import me.eccentric_nz.TARDIS.builders.TARDISInteriorPostioning;
+import me.eccentric_nz.TARDIS.builders.TARDISTIPSData;
 import me.eccentric_nz.TARDIS.database.ResultSetTardis;
 import me.eccentric_nz.TARDIS.desktop.TARDISThemeInventory;
 import me.eccentric_nz.TARDIS.desktop.TARDISUpgradeData;
 import me.eccentric_nz.TARDIS.enumeration.SCHEMATIC;
 import me.eccentric_nz.TARDIS.utility.TARDISMessage;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
@@ -55,6 +58,28 @@ public class TARDISUpgradeCommand {
         // console must in a TARDIS world
         if (!plugin.getUtils().canGrowRooms(rs.getChunk())) {
             TARDISMessage.send(player, "UPGRADE_ABORT_WORLD");
+            return true;
+        }
+        // it must be their own TARDIS
+        boolean own;
+        Location pl = player.getLocation();
+        String current_world = pl.getWorld().getName();
+        String[] split = rs.getChunk().split(":");
+        String db_world = split[0];
+        if (plugin.getConfig().getBoolean("creation.default_world")) {
+            if (plugin.getConfig().getBoolean("creation.create_worlds_with_perms") && player.hasPermission("tardis.create_world")) {
+                own = (current_world.equals(db_world));
+            } else {
+                // get if player is in TIPS area for their TARDIS
+                TARDISInteriorPostioning tintpos = new TARDISInteriorPostioning(plugin);
+                TARDISTIPSData pos = tintpos.getTIPSData(rs.getTIPS());
+                own = (pl.getBlockX() > pos.getMinX() && pl.getBlockZ() > pos.getMinZ() && pl.getBlockX() < pos.getMaxX() && pl.getBlockZ() < pos.getMaxZ());
+            }
+        } else {
+            own = (current_world.equals(db_world));
+        }
+        if (!own) {
+            TARDISMessage.send(player, "NOT_OWNER");
             return true;
         }
         // get player's current console
