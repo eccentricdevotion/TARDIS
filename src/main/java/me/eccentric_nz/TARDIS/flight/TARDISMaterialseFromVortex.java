80ab4e2536203d46fc223db9a5c5dc1e313e0c74
eccentricdevotion <eccentricdevotion@gmail.com>
2016-06-19 10:22:39 +1200

    Properly set the delays when in non-normal flight mode

--- src/main/java/me/eccentric_nz/TARDIS/flight/TARDISMaterialseFromVortex.java
+++ src/main/java/me/eccentric_nz/TARDIS/flight/TARDISMaterialseFromVortex.java
@@ -162,13 +162,14 @@ public class TARDISMaterialseFromVortex implements Runnable {
 
                     // remember flight data
                     plugin.getTrackerKeeper().getFlightData().put(uuid, bd);
-                    long delay = (plugin.getTrackerKeeper().getDestinationVortex().containsKey(id)) ? 0L : 500L;
+                    long flight_mode_delay = (plugin.getTrackerKeeper().getDestinationVortex().containsKey(id)) ? 0L : 500L;
+                    long materialisation_delay = flight_mode_delay;
                     // flight mode
                     if (flight_mode == 2 || flight_mode == 3) {
-                        delay += 650L;
+                        materialisation_delay += 650L;
                         Runnable runner = (flight_mode == 2) ? new TARDISRegulatorStarter(plugin, player) : new TARDISManualFlightStarter(plugin, player, id);
-                        // start the flying mode
-                        plugin.getServer().getScheduler().runTask(plugin, runner);
+                        // start the flying mode (after demat if not in vortex already)
+                        plugin.getServer().getScheduler().scheduleSyncDelayedTask(plugin, runner, flight_mode_delay);
                     }
                     final boolean mine_sound = minecart;
                     final Location sound_loc = (preset.equals(PRESET.JUNK_MODE)) ? exit : handbrake;
@@ -241,7 +242,7 @@ public class TARDISMaterialseFromVortex implements Runnable {
                                 plugin.getTrackerKeeper().getFlightData().remove(uuid);
                             }
                         }
-                    }, delay);
+                    }, materialisation_delay);
                     if (plugin.getTrackerKeeper().getDamage().containsKey(id)) {
                         plugin.getTrackerKeeper().getDamage().remove(id);
                     }
