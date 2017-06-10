239b1c57cec1cba3b21732da256746474762cd0c
eccentricdevotion <eccentricdevotion@gmail.com>
2017-06-10 21:36:58 +1200

    Make give success message generic for bow ties and jelly babies

--- src/main/java/me/eccentric_nz/TARDIS/commands/admin/TARDISGiveCommand.java
+++ src/main/java/me/eccentric_nz/TARDIS/commands/admin/TARDISGiveCommand.java
@@ -389,6 +389,7 @@ public class TARDISGiveCommand implements CommandExecutor {
         String item_to_give = items.get(item);
         ItemStack book = new ItemStack(Material.KNOWLEDGE_BOOK, 1);
         KnowledgeBookMeta kbm = (KnowledgeBookMeta) book.getItemMeta();
+        String message = item_to_give;
         switch (item) {
             case "bow-tie":
                 List<String> colours = Arrays.asList("White", "Orange", "Magenta", "Light_Blue", "Yellow", "Lime", "Pink", "Grey", "Light_Grey", "Cyan", "Purple", "Blue", "Brown", "Green", "Red", "Black");
@@ -396,6 +397,7 @@ public class TARDISGiveCommand implements CommandExecutor {
                     NamespacedKey nsk = new NamespacedKey(plugin, bt + "_Bow_Tie");
                     kbm.addRecipe(nsk);
                 });
+                message = "Bow Ties";
                 break;
             case "jelly-baby":
                 List<String> flavours = Arrays.asList("Vanilla", "Orange", "Watermelon", "Bubblegum", "Lemon", "Lime", "Strawberry", "Earl_Grey", "Vodka", "Island_Punch", "Grape", "Blueberry", "Cappuccino", "Apple", "Raspberry", "Licorice");
@@ -403,6 +405,7 @@ public class TARDISGiveCommand implements CommandExecutor {
                     NamespacedKey nsk = new NamespacedKey(plugin, jelly + "_Jelly_Baby");
                     kbm.addRecipe(nsk);
                 });
+                message = "Jelly Babies";
                 break;
             default:
                 NamespacedKey nsk = new NamespacedKey(plugin, item_to_give.replace(" ", "_"));
@@ -411,7 +414,7 @@ public class TARDISGiveCommand implements CommandExecutor {
         book.setItemMeta(kbm);
         player.getInventory().addItem(book);
         player.updateInventory();
-        TARDISMessage.send(player, "GIVE_KNOWLEDGE", sender.getName(), item_to_give);
+        TARDISMessage.send(player, "GIVE_KNOWLEDGE", sender.getName(), message);
         return true;
     }
 }
