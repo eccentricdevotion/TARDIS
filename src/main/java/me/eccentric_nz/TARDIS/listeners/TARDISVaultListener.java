4a75dee01d4419e60f1415a3e3d93719c332be15
eccentricdevotion <eccentricdevotion@gmail.com>
2018-04-19 16:51:49 +1200

    Try to reduce poor timings caused by the vault checker

--- src/main/java/me/eccentric_nz/TARDIS/listeners/TARDISVaultListener.java
+++ src/main/java/me/eccentric_nz/TARDIS/listeners/TARDISVaultListener.java
@@ -5,6 +5,7 @@ import java.util.List;
 import me.eccentric_nz.TARDIS.TARDIS;
 import me.eccentric_nz.TARDIS.database.ResultSetVault;
 import me.eccentric_nz.TARDIS.sonic.TARDISSonicSorterListener;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.block.Block;
@@ -32,59 +33,62 @@ public class TARDISVaultListener implements Listener {
         InventoryHolder holder = inv.getHolder();
         if (holder instanceof Chest) {
             Chest chest = (Chest) holder;
-            String loc = chest.getLocation().toString();
-            // check is drop chest
-            ResultSetVault rs = new ResultSetVault(plugin, loc);
-            if (!rs.resultSet()) {
-                return;
-            }
-            // sort contents
-            TARDISSonicSorterListener.sortInventory(inv, 0, inv.getSize());
-            World w = chest.getWorld();
-            // get vault dimensions
-            int sx = rs.getX();
-            int sy = rs.getY();
-            int sz = rs.getZ();
-            // loop through vault blocks
-            for (int y = sy; y < (sy + 16); y++) {
-                for (int x = sx; x < (sx + 16); x++) {
-                    for (int z = sz; z < (sz + 16); z++) {
-                        // get the block
-                        Block b = w.getBlockAt(x, y, z);
-                        // check if it is a chest (but not the drop chest)
-                        if ((b.getType().equals(Material.CHEST) || b.getType().equals(Material.TRAPPED_CHEST)) && !loc.equals(b.getLocation().toString())) {
-                            Chest c = (Chest) b.getState();
-                            // get chest contents
-                            Inventory cinv = c.getBlockInventory();
-                            // make sure there is a free slot
-                            if (cinv.firstEmpty() != -1) {
-                                ItemStack[] cc = cinv.getContents();
-                                List<Material> mats = new ArrayList<>();
-                                // find unique item stack materials
-                                for (ItemStack is : cc) {
-                                    if (is != null) {
-                                        Material m = is.getType();
-                                        if (!mats.contains(m)) {
-                                            mats.add(m);
+            Location l = chest.getLocation();
+            if (plugin.getUtils().inTARDISWorld(l)) {
+                String loc = l.toString();
+                // check is drop chest
+                ResultSetVault rs = new ResultSetVault(plugin, loc);
+                if (!rs.resultSet()) {
+                    return;
+                }
+                // sort contents
+                TARDISSonicSorterListener.sortInventory(inv, 0, inv.getSize());
+                World w = chest.getWorld();
+                // get vault dimensions
+                int sx = rs.getX();
+                int sy = rs.getY();
+                int sz = rs.getZ();
+                // loop through vault blocks
+                for (int y = sy; y < (sy + 16); y++) {
+                    for (int x = sx; x < (sx + 16); x++) {
+                        for (int z = sz; z < (sz + 16); z++) {
+                            // get the block
+                            Block b = w.getBlockAt(x, y, z);
+                            // check if it is a chest (but not the drop chest)
+                            if ((b.getType().equals(Material.CHEST) || b.getType().equals(Material.TRAPPED_CHEST)) && !loc.equals(b.getLocation().toString())) {
+                                Chest c = (Chest) b.getState();
+                                // get chest contents
+                                Inventory cinv = c.getBlockInventory();
+                                // make sure there is a free slot
+                                if (cinv.firstEmpty() != -1) {
+                                    ItemStack[] cc = cinv.getContents();
+                                    List<Material> mats = new ArrayList<>();
+                                    // find unique item stack materials
+                                    for (ItemStack is : cc) {
+                                        if (is != null) {
+                                            Material m = is.getType();
+                                            if (!mats.contains(m)) {
+                                                mats.add(m);
+                                            }
                                         }
                                     }
+                                    // for each material found, see if there are any stacks of it in the drop chest
+                                    mats.forEach((m) -> {
+                                        int slot = inv.first(m);
+                                        while (slot != -1 && cinv.firstEmpty() != -1) {
+                                            // get the item stack
+                                            ItemStack get = inv.getItem(slot);
+                                            // remove the stack from the drop chest
+                                            inv.setItem(slot, null);
+                                            // put it in the chest
+                                            cinv.setItem(cinv.firstEmpty(), get);
+                                            // sort the chest
+                                            TARDISSonicSorterListener.sortInventory(cinv, 0, cinv.getSize());
+                                            // get any other stacks
+                                            slot = inv.first(m);
+                                        }
+                                    });
                                 }
-                                // for each material found, see if there are any stacks of it in the drop chest
-                                mats.forEach((m) -> {
-                                    int slot = inv.first(m);
-                                    while (slot != -1 && cinv.firstEmpty() != -1) {
-                                        // get the item stack
-                                        ItemStack get = inv.getItem(slot);
-                                        // remove the stack from the drop chest
-                                        inv.setItem(slot, null);
-                                        // put it in the chest
-                                        cinv.setItem(cinv.firstEmpty(), get);
-                                        // sort the chest
-                                        TARDISSonicSorterListener.sortInventory(cinv, 0, cinv.getSize());
-                                        // get any other stacks
-                                        slot = inv.first(m);
-                                    }
-                                });
                             }
                         }
                     }
