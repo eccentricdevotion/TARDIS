286c81409d104009ec3a5cf7524f125895b49965
eccentricdevotion <eccentricdevotion@gmail.com>
2018-08-12 16:43:00 +1200

    New village finder - requires TARDISChunkGenerator v4.0.2

--- src/main/java/me/eccentric_nz/TARDIS/travel/TARDISVillageTravel.java
+++ src/main/java/me/eccentric_nz/TARDIS/travel/TARDISVillageTravel.java
@@ -19,9 +19,7 @@ package me.eccentric_nz.TARDIS.travel;
 import me.eccentric_nz.TARDIS.TARDIS;
 import me.eccentric_nz.TARDIS.TARDISConstants;
 import me.eccentric_nz.TARDIS.database.ResultSetCurrentLocation;
-import me.eccentric_nz.TARDIS.travel.TARDISNbtFactory.NbtCompound;
 import me.eccentric_nz.TARDIS.utility.TARDISMessage;
-import me.eccentric_nz.TARDIS.utility.TARDISNumberParsers;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.World.Environment;
@@ -29,15 +27,8 @@ import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Random;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * @author eccentric_nz
@@ -67,57 +58,23 @@ public class TARDISVillageTravel {
                 TARDISMessage.send(p, "VILLAGE_NO_END");
                 return null;
             }
-            // get path to world data folder
-            File container = plugin.getServer().getWorldContainer();
-            // check for MCPC+
-            Pattern pat = Pattern.compile("MCPC", Pattern.DOTALL);
-            Matcher mat = pat.matcher(plugin.getServer().getVersion());
-            String server_world;
-            if (mat.find()) {
-                server_world = "data" + File.separator;
-            } else {
-                server_world = world.getName() + File.separator + "data" + File.separator;
-            }
-            String root = container.getAbsolutePath() + File.separator + server_world;
-            File file = new File(root, "Village.dat");
-            if (!file.exists()) {
-                plugin.debug("Could not get Village.dat for world: " + world.getName());
-                return null;
-            }
-            String path = root + "Village.dat";
-            try {
-                FileInputStream is = new FileInputStream(path);
-                NbtCompound nbtc = TARDISNbtFactory.fromCompressedStream(is);
-                NbtCompound nbtdata = nbtc.getPath("data");
-                NbtCompound nbtf = nbtdata.getPath("Features");
-                List<String> nbtl = new ArrayList<>(nbtf.keySet());
-                int size = nbtl.size();
-                String r = nbtl.get(rand.nextInt(size));
-                String[] split = r.substring(1, r.length() - 1).split(",");
-                int x = TARDISNumberParsers.parseInt(split[0]) * 16;
-                int z = TARDISNumberParsers.parseInt(split[1]) * 16;
-                int y = world.getHighestBlockYAt(x, z);
-                Location loc = new Location(world, x, y, z);
-                // check for space
-                Block b = loc.getBlock();
-                boolean unsafe = true;
-                while (unsafe) {
-                    boolean clear = true;
-                    for (BlockFace f : plugin.getGeneralKeeper().getSurrounding()) {
-                        if (!TARDISConstants.GOOD_MATERIALS.contains(b.getRelative(f).getType())) {
-                            b = b.getRelative(BlockFace.UP);
-                            clear = false;
-                            break;
-                        }
+            Location loc = plugin.getTardisHelper().getRandomVillage(world);
+            // check for space
+            Block b = loc.getBlock();
+            boolean unsafe = true;
+            while (unsafe) {
+                boolean clear = true;
+                for (BlockFace f : plugin.getGeneralKeeper().getSurrounding()) {
+                    if (!TARDISConstants.GOOD_MATERIALS.contains(b.getRelative(f).getType())) {
+                        b = b.getRelative(BlockFace.UP);
+                        clear = false;
+                        break;
                     }
-                    unsafe = !clear;
                 }
-                loc.setY(b.getY());
-                return loc;
-            } catch (IOException ex) {
-                plugin.debug("Could not get Village.dat: " + ex.getMessage());
-                return null;
+                unsafe = !clear;
             }
+            loc.setY(b.getY());
+            return loc;
         } else {
             TARDISMessage.send(p, "CURRENT_NOT_FOUND");
             return null;
