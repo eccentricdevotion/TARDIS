078f155f5e78c85818237cfc3b9327e3513a6cd5
eccentricdevotion <eccentricdevotion@gmail.com>
2019-04-28 14:04:36 +1200

    TARDIS can set the spawn locations for its own worlds

--- src/main/java/me/eccentric_nz/TARDIS/builders/TARDISBuilderInner.java
+++ src/main/java/me/eccentric_nz/TARDIS/builders/TARDISBuilderInner.java
@@ -25,7 +25,6 @@ import me.eccentric_nz.TARDIS.database.QueryFactory;
 import me.eccentric_nz.TARDIS.database.ResultSetAchievements;
 import me.eccentric_nz.TARDIS.enumeration.SCHEMATIC;
 import me.eccentric_nz.TARDIS.enumeration.USE_CLAY;
-import me.eccentric_nz.TARDIS.enumeration.WORLD_MANAGER;
 import me.eccentric_nz.TARDIS.schematic.TARDISBannerSetter;
 import me.eccentric_nz.TARDIS.schematic.TARDISSchematicGZip;
 import me.eccentric_nz.TARDIS.utility.*;
@@ -304,11 +303,7 @@ public class TARDISBuilderInner {
                             qf.doInsert("doors", setd);
                             // if create_worlds is true, set the world spawn
                             if (own_world) {
-                                if (plugin.getWorldManager().equals(WORLD_MANAGER.MULTIVERSE)) {
-                                    plugin.getMVHelper().setSpawnLocation(world, x, y, z);
-                                } else {
-                                    world.setSpawnLocation(x, y, (z + 1));
-                                }
+                                world.setSpawnLocation(x, y, (z + 1));
                             }
                         }
                     }
