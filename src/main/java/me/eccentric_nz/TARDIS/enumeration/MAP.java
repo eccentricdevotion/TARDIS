f8bfe8be0b50b4f28a59c8bc40ee7e724c5741c5
eccentricdevotion <eccentricdevotion@gmail.com>
2019-05-20 15:30:32 +1200

    Update copyright year to 2019

--- src/main/java/me/eccentric_nz/TARDIS/enumeration/MAP.java
+++ src/main/java/me/eccentric_nz/TARDIS/enumeration/MAP.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 eccentric_nz
+ * Copyright (C) 2019 eccentric_nz
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -20,7 +20,7 @@ package me.eccentric_nz.TARDIS.enumeration;
  * @author eccentric_nz
  */
 public enum MAP {
-    
+
     ADMIN("Server Admin Circuit"),
     ARS("TARDIS ARS Circuit"),
     BIO("Bio-scanner Circuit"),
