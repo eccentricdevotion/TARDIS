b2560c6f88d56467bf0cd1b55c1c67ab3dc9824c
eccentricdevotion <eccentricdevotion@gmail.com>
2019-07-13 15:40:23 +1200

    Catch NPE in constructs converter

--- src/main/java/me/eccentric_nz/TARDIS/chameleon/ConstructsConverter.java
+++ src/main/java/me/eccentric_nz/TARDIS/chameleon/ConstructsConverter.java
@@ -48,109 +48,111 @@ public class ConstructsConverter {
                     String bpID = rs.getString("blueprintID");
                     String bpData = rs.getString("blueprintData");
                     String glID = rs.getString("glassID");
-                    Matcher m = p.matcher(bpID);
-                    if (!m.find()) {
-                        // no IDs found, already converted
-                        plugin.debug("doesn't match");
-                        continue;
-                    }
-                    String[][] bpGrid = new String[10][4];
-                    String[][] stGrid = new String[10][4];
-                    String[][] glGrid = new String[10][4];
-                    JSONArray bpIDJson = new JSONArray(bpID);
-                    JSONArray bpDataJson = new JSONArray(bpData);
-                    JSONArray glIDJson = new JSONArray(glID);
-                    for (int y = 0; y < 10; y++) {
-                        JSONArray bpIDX = bpIDJson.getJSONArray(y);
-                        JSONArray bpDATAX = bpDataJson.getJSONArray(y);
-                        JSONArray glIDX = glIDJson.getJSONArray(y);
-                        for (int x = 0; x < 4; x++) {
-                            Material material = tmic.LEGACY_ID_LOOKUP.get(bpIDX.getInt(x));
-                            bpGrid[y][x] = material.createBlockData().getAsString();
-                            switch (material) {
-                                case WHITE_CARPET:
-                                case WHITE_STAINED_GLASS:
-                                case WHITE_STAINED_GLASS_PANE:
-                                case WHITE_WOOL:
-                                case GREEN_TERRACOTTA:
-                                    // get correct colour
-                                    String[] split = material.toString().split("_");
-                                    split[0] = tmic.COLOUR_LOOKUP.get(bpDATAX.getInt(x));
-                                    StringBuilder sb = new StringBuilder();
-                                    for (int s = 0; s < split.length; s++) {
-                                        sb.append(split[s]);
-                                        if (s != split.length - 1) {
-                                            sb.append("_");
-                                        }
-                                    }
-                                    String implode = sb.toString();
-                                    material = Material.valueOf(implode);
-                                    bpGrid[y][x] = material.createBlockData().getAsString();
-                                    stGrid[y][x] = plugin.getBuildKeeper().getStainedGlassLookup().getStain().get(material).createBlockData().getAsString();
-                                    break;
-                                case ACACIA_DOOR:
-                                case BIRCH_DOOR:
-                                case DARK_OAK_DOOR:
-                                case IRON_DOOR:
-                                case JUNGLE_DOOR:
-                                case OAK_DOOR:
-                                case SPRUCE_DOOR:
-                                    BlockData dbd = material.createBlockData();
-                                    Door door = (Door) dbd;
-                                    // set facing / hinge
-                                    door.setFacing(BlockFace.EAST);
-                                    door.setHinge(Door.Hinge.RIGHT);
-                                    if (x == 0) {
-                                        // bottom door
-                                        door.setHalf(Bisected.Half.BOTTOM);
-                                    }
-                                    if (x == 1) {
-                                        // top door
-                                        door.setHalf(Bisected.Half.TOP);
-                                    }
-                                    String doorData = door.getAsString();
-                                    bpGrid[y][x] = doorData;
-                                    stGrid[y][x] = doorData;
-                                    break;
-                                case ACACIA_SIGN:
-                                case ACACIA_WALL_SIGN:
-                                case AIR:
-                                case BIRCH_SIGN:
-                                case BIRCH_WALL_SIGN:
-                                case DARK_OAK_SIGN:
-                                case DARK_OAK_WALL_SIGN:
-                                case JUNGLE_SIGN:
-                                case JUNGLE_WALL_SIGN:
-                                case OAK_SIGN:
-                                case OAK_WALL_SIGN:
-                                case REDSTONE_TORCH:
-                                case REDSTONE_WALL_TORCH:
-                                case SPRUCE_SIGN:
-                                case SPRUCE_WALL_SIGN:
-                                case TORCH:
-                                case WALL_TORCH:
-                                    stGrid[y][x] = tmic.LEGACY_ID_LOOKUP.get(bpIDX.getInt(x)).createBlockData().getAsString();
-                                    break;
-                                default:
-                                    try {
-                                        stGrid[y][x] = plugin.getBuildKeeper().getStainedGlassLookup().getStain().get(material).createBlockData().getAsString();
-                                    } catch (NullPointerException ex) {
-                                        plugin.debug(material.toString());
-                                    }
-                                    break;
-                            }
-                            glGrid[y][x] = tmic.LEGACY_ID_LOOKUP.get(glIDX.getInt(x)).createBlockData().getAsString();
+                    if (bpID != null) {
+                        Matcher m = p.matcher(bpID);
+                        if (!m.find()) {
+                            // no IDs found, already converted
+                            plugin.debug("doesn't match");
+                            continue;
                         }
+                        String[][] bpGrid = new String[10][4];
+                        String[][] stGrid = new String[10][4];
+                        String[][] glGrid = new String[10][4];
+                        JSONArray bpIDJson = new JSONArray(bpID);
+                        JSONArray bpDataJson = new JSONArray(bpData);
+                        JSONArray glIDJson = new JSONArray(glID);
+                        for (int y = 0; y < 10; y++) {
+                            JSONArray bpIDX = bpIDJson.getJSONArray(y);
+                            JSONArray bpDATAX = bpDataJson.getJSONArray(y);
+                            JSONArray glIDX = glIDJson.getJSONArray(y);
+                            for (int x = 0; x < 4; x++) {
+                                Material material = tmic.LEGACY_ID_LOOKUP.get(bpIDX.getInt(x));
+                                bpGrid[y][x] = material.createBlockData().getAsString();
+                                switch (material) {
+                                    case WHITE_CARPET:
+                                    case WHITE_STAINED_GLASS:
+                                    case WHITE_STAINED_GLASS_PANE:
+                                    case WHITE_WOOL:
+                                    case GREEN_TERRACOTTA:
+                                        // get correct colour
+                                        String[] split = material.toString().split("_");
+                                        split[0] = tmic.COLOUR_LOOKUP.get(bpDATAX.getInt(x));
+                                        StringBuilder sb = new StringBuilder();
+                                        for (int s = 0; s < split.length; s++) {
+                                            sb.append(split[s]);
+                                            if (s != split.length - 1) {
+                                                sb.append("_");
+                                            }
+                                        }
+                                        String implode = sb.toString();
+                                        material = Material.valueOf(implode);
+                                        bpGrid[y][x] = material.createBlockData().getAsString();
+                                        stGrid[y][x] = plugin.getBuildKeeper().getStainedGlassLookup().getStain().get(material).createBlockData().getAsString();
+                                        break;
+                                    case ACACIA_DOOR:
+                                    case BIRCH_DOOR:
+                                    case DARK_OAK_DOOR:
+                                    case IRON_DOOR:
+                                    case JUNGLE_DOOR:
+                                    case OAK_DOOR:
+                                    case SPRUCE_DOOR:
+                                        BlockData dbd = material.createBlockData();
+                                        Door door = (Door) dbd;
+                                        // set facing / hinge
+                                        door.setFacing(BlockFace.EAST);
+                                        door.setHinge(Door.Hinge.RIGHT);
+                                        if (x == 0) {
+                                            // bottom door
+                                            door.setHalf(Bisected.Half.BOTTOM);
+                                        }
+                                        if (x == 1) {
+                                            // top door
+                                            door.setHalf(Bisected.Half.TOP);
+                                        }
+                                        String doorData = door.getAsString();
+                                        bpGrid[y][x] = doorData;
+                                        stGrid[y][x] = doorData;
+                                        break;
+                                    case ACACIA_SIGN:
+                                    case ACACIA_WALL_SIGN:
+                                    case AIR:
+                                    case BIRCH_SIGN:
+                                    case BIRCH_WALL_SIGN:
+                                    case DARK_OAK_SIGN:
+                                    case DARK_OAK_WALL_SIGN:
+                                    case JUNGLE_SIGN:
+                                    case JUNGLE_WALL_SIGN:
+                                    case OAK_SIGN:
+                                    case OAK_WALL_SIGN:
+                                    case REDSTONE_TORCH:
+                                    case REDSTONE_WALL_TORCH:
+                                    case SPRUCE_SIGN:
+                                    case SPRUCE_WALL_SIGN:
+                                    case TORCH:
+                                    case WALL_TORCH:
+                                        stGrid[y][x] = tmic.LEGACY_ID_LOOKUP.get(bpIDX.getInt(x)).createBlockData().getAsString();
+                                        break;
+                                    default:
+                                        try {
+                                            stGrid[y][x] = plugin.getBuildKeeper().getStainedGlassLookup().getStain().get(material).createBlockData().getAsString();
+                                        } catch (NullPointerException ex) {
+                                            plugin.debug(material.toString());
+                                        }
+                                        break;
+                                }
+                                glGrid[y][x] = tmic.LEGACY_ID_LOOKUP.get(glIDX.getInt(x)).createBlockData().getAsString();
+                            }
+                        }
+                        JSONArray bpArr = new JSONArray(bpGrid);
+                        JSONArray stArr = new JSONArray(stGrid);
+                        JSONArray glArr = new JSONArray(glGrid);
+                        update.setString(1, bpArr.toString());
+                        update.setString(2, stArr.toString());
+                        update.setString(3, glArr.toString());
+                        update.setInt(4, id);
+                        update.addBatch();
+                        i++;
                     }
-                    JSONArray bpArr = new JSONArray(bpGrid);
-                    JSONArray stArr = new JSONArray(stGrid);
-                    JSONArray glArr = new JSONArray(glGrid);
-                    update.setString(1, bpArr.toString());
-                    update.setString(2, stArr.toString());
-                    update.setString(3, glArr.toString());
-                    update.setInt(4, id);
-                    update.addBatch();
-                    i++;
                 }
             }
             if (i > 0) {
