b0faf2b2e8eb801ec4b1846c7125acc08f8f905d
eccentricdevotion <eccentricdevotion@gmail.com>
2020-02-09 14:05:18 +1300

    Desktop theme condenser record needs to go in the correct table

--- src/main/java/me/eccentric_nz/TARDIS/desktop/TARDISThemeRepairRunnable.java
+++ src/main/java/me/eccentric_nz/TARDIS/desktop/TARDISThemeRepairRunnable.java
@@ -399,8 +399,8 @@ public class TARDISThemeRepairRunnable extends TARDISThemeRunnable {
                 }
                 if (type.equals(Material.CHEST)) {
                     // remember the location of the condenser chest
-                    String chest = world.getName() + ":" + x + ":" + y + ":" + z;
-                    set.put("condenser", chest);
+                    String condenser = TARDISStaticLocationGetters.makeLocationStr(world, x, y, z);
+                    plugin.getQueryFactory().insertSyncControl(id, 34, condenser, 0);
                 }
                 if (type.equals(Material.IRON_DOOR)) {
                     Bisected bisected = (Bisected) data;
