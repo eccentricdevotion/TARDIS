01c53aceb47dd6fc9a5bf0d6550a1e5e3089c3a6
eccentricdevotion <eccentricdevotion@gmail.com>
2020-05-25 21:31:06 +1200

    Remove Project-Rassilon dependency it's not been updated since 2016

--- src/main/java/me/eccentric_nz/TARDIS/TARDISListenerRegisterer.java
+++ src/main/java/me/eccentric_nz/TARDIS/TARDISListenerRegisterer.java
@@ -198,9 +198,6 @@ class TARDISListenerRegisterer {
         plugin.getPM().registerEvents(new TARDISPresetListener(plugin), plugin);
         plugin.getPM().registerEvents(new TARDISQuitListener(plugin), plugin);
         plugin.getPM().registerEvents(new TARDISRecipeListener(plugin), plugin);
-        if (plugin.getPM().isPluginEnabled("ProjectRassilon")) {
-            plugin.getPM().registerEvents(new TARDISRegenerationListener(plugin), plugin);
-        }
         plugin.getPM().registerEvents(new TARDISRegulatorListener(plugin), plugin);
         plugin.getPM().registerEvents(new TARDISRemoteKeyListener(plugin), plugin);
         plugin.getPM().registerEvents(new TARDISRedstoneListener(plugin), plugin);
