5dd42dc73703d009d0e708125302ae4100f2b529
eccentricdevotion <eccentricdevotion@gmail.com>
2020-06-01 22:20:21 +1200

    Add missing options to default config so they don't have to be added on first run
    + remove 1 unused option

--- src/main/java/me/eccentric_nz/TARDIS/commands/admin/TARDISAdminCommands.java
+++ src/main/java/me/eccentric_nz/TARDIS/commands/admin/TARDISAdminCommands.java
@@ -158,7 +158,6 @@ public class TARDISAdminCommands implements CommandExecutor {
         firstsBool.put("rooms_require_blocks", "growth");
         firstsBool.put("set_biome", "police_box");
         firstsBool.put("sfx", "allow");
-        firstsBool.put("sky_biome", "creation");
         firstsBool.put("spawn_eggs", "allow");
         firstsBool.put("spawn_random_monsters", "preferences");
         firstsBool.put("strike_lightning", "preferences");
