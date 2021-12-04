219f9287b923b9dc9aaf9d58b583949afdf132b5
Eccentric Devotion <eccentricdevotion@gmail.com>
2021-12-05 12:23:34 +1300

    Bump version to 4.8.1, require TARDISChunkGenerator 4.8.1

--- src/main/java/me/eccentric_nz/TARDIS/TARDIS.java
+++ src/main/java/me/eccentric_nz/TARDIS/TARDIS.java
@@ -189,7 +189,7 @@ public class TARDIS extends JavaPlugin {
         versions.put("MultiInv", "3.3.6");
         versions.put("My_Worlds", "1.16.1");
         versions.put("PerWorldInventory", "2.3.0");
-        versions.put("TARDISChunkGenerator", "4.8.0");
+        versions.put("TARDISChunkGenerator", "4.8.1");
         versions.put("Towny", "0.95");
         versions.put("WorldBorder", "1.9.0");
         versions.put("WorldGuard", "7.0.5");
