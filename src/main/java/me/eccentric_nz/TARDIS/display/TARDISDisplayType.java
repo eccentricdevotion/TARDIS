ca55afa25caabe740a7613c1389394bc96150d4f
Eccentric Devotion <eccentricdevotion@gmail.com>
2022-12-05 17:45:12 +1300

    Add %WORLD% placeholder to TARDIS display

--- src/main/java/me/eccentric_nz/TARDIS/display/TARDISDisplayType.java
+++ src/main/java/me/eccentric_nz/TARDIS/display/TARDISDisplayType.java
@@ -22,7 +22,8 @@ public enum TARDISDisplayType {
     BIOME("&6B&7%BIOME%"),
     COORDS("&6X&7%X% &6Y&7%Y% &6Z&7%Z%"),
     DIRECTION("&6F&7%FACING% (%FACING_XZ%)"),
-    TARGET_BLOCK("&6T&7%TARGET_BLOCK%");
+    TARGET_BLOCK("&6T&7%TARGET_BLOCK%"),
+    WORLD("&6W&7%WORLD%");
 
     private final String format;
 
