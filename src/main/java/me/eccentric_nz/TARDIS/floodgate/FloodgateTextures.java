a47af73f7e98467a2a7da9fa328dde68df3be4d8
eccentricdevotion <eccentricdevotion@gmail.com>
2023-02-16 21:55:58 +1300

    Create FloodgateTextures.java

---
+++ src/main/java/me/eccentric_nz/TARDIS/floodgate/FloodgateTextures.java
@@ -0,0 +1,61 @@
+package me.eccentric_nz.TARDIS.floodgate;
+
+import java.util.HashMap;
+
+public class FloodgateTextures {
+
+    public static final HashMap<String, String> lookup = new HashMap<>();
+
+    static {
+        lookup.put("andesite", "stone_andesite");
+        lookup.put("blackstone", "blackstone");
+        lookup.put("blue_wool", "wool_colored_blue");
+        lookup.put("book", "");
+        lookup.put("bricks", "");
+        lookup.put("brown_mushroom_block", "");
+        lookup.put("brown_wool", "wool_colored_brown");
+        lookup.put("chorus_flower", "chorus_flower");
+        lookup.put("coal_block", "coal_block");
+        lookup.put("cobblestone", "cobblestone");
+        lookup.put("cobweb", "");
+        lookup.put("diorite", "stone_diorite");
+        lookup.put("dirt", "dirt");
+        lookup.put("end_stone", "end_stone");
+        lookup.put("ender_chest", "");
+        lookup.put("feather", "");
+        lookup.put("glowstone", "glowstone");
+        lookup.put("granite", "stone_granite");
+        lookup.put("green_wool", "wool_colored_green");
+        lookup.put("hopper", "");
+        lookup.put("iron_bars", "iron_bars");
+        lookup.put("iron_block", "iron_block");
+        lookup.put("iron_ingot", "");
+        lookup.put("iron_trapdoor", "iron_trapdoor");
+        lookup.put("lantern", "");
+        lookup.put("mossy_cobblestone", "cobblestone_mossy");
+        lookup.put("mossy_stone_bricks", "stonebrick_mossy");
+        lookup.put("nether_bricks", "");
+        lookup.put("nether_quartz_ore", "");
+        lookup.put("netherrack", "netherrack");
+        lookup.put("oak_fence", "");
+        lookup.put("oak_log", "log_oak");
+        lookup.put("orange_wool", "wool_colored_orange");
+        lookup.put("pink_wool", "wool_colored_pink");
+        lookup.put("prismarine", "");
+        lookup.put("quartz_block", "");
+        lookup.put("rail", "rail_normal");
+        lookup.put("red_terracotta", "hardened_clay_stained_red");
+        lookup.put("red_wool", "wool_colored_red");
+        lookup.put("sandstone", "");
+        lookup.put("snow_block", "");
+        lookup.put("spruce_leaves", "");
+        lookup.put("stone_brick_stairs", "");
+        lookup.put("stone_bricks", "");
+        lookup.put("terracotta", "hardened_clay");
+        lookup.put("white_wool", "wool_colored_white");
+        lookup.put("yellow_concrete", "");
+        lookup.put("yellow_wool", "wool_colored_yellow");
+
+    }
+
+}
