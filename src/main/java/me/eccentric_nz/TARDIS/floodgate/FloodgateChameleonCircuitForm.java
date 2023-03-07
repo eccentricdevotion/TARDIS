112dec309cfc08deb984347ea8f0f7c39a9fdf36
eccentricdevotion <eccentricdevotion@gmail.com>
2023-03-07 20:15:48 +1300

    #700 Add Lock button to floodgate Chameleon form

--- src/main/java/me/eccentric_nz/TARDIS/floodgate/FloodgateChameleonCircuitForm.java
+++ src/main/java/me/eccentric_nz/TARDIS/floodgate/FloodgateChameleonCircuitForm.java
@@ -1,10 +1,16 @@
 package me.eccentric_nz.TARDIS.floodgate;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.UUID;
 import me.eccentric_nz.TARDIS.TARDIS;
 import me.eccentric_nz.TARDIS.advanced.TARDISCircuitChecker;
 import me.eccentric_nz.TARDIS.advanced.TARDISCircuitDamager;
 import me.eccentric_nz.TARDIS.chameleon.TARDISChameleonFrame;
 import me.eccentric_nz.TARDIS.database.resultset.ResultSetControls;
+import me.eccentric_nz.TARDIS.database.resultset.ResultSetCurrentLocation;
+import me.eccentric_nz.TARDIS.database.resultset.ResultSetTardis;
+import me.eccentric_nz.TARDIS.enumeration.Adaption;
 import me.eccentric_nz.TARDIS.enumeration.Control;
 import me.eccentric_nz.TARDIS.enumeration.Difficulty;
 import me.eccentric_nz.TARDIS.enumeration.DiskCircuit;
@@ -12,16 +18,14 @@ import me.eccentric_nz.TARDIS.enumeration.PRESET;
 import me.eccentric_nz.TARDIS.messaging.TARDISMessage;
 import me.eccentric_nz.TARDIS.utility.TARDISStaticUtils;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.block.Biome;
 import org.bukkit.entity.Player;
 import org.geysermc.cumulus.form.SimpleForm;
 import org.geysermc.cumulus.response.SimpleFormResponse;
 import org.geysermc.cumulus.util.FormImage;
 import org.geysermc.floodgate.api.FloodgateApi;
 import org.geysermc.floodgate.api.player.FloodgatePlayer;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.UUID;
 
 public class FloodgateChameleonCircuitForm {
 
@@ -47,6 +51,7 @@ public class FloodgateChameleonCircuitForm {
                 .button("Invisible", FormImage.Type.URL, "https://github.com/eccentricdevotion/TARDIS-Resource-Pack/raw/master/assets/tardis/textures/item/gui/control/invisible_button.png")
                 .button("Shorted out", FormImage.Type.URL, "https://github.com/eccentricdevotion/TARDIS-Resource-Pack/raw/master/assets/tardis/textures/item/gui/chameleon/shorted_button.png")
                 .button("Construct", FormImage.Type.URL, "https://github.com/eccentricdevotion/TARDIS-Resource-Pack/raw/master/assets/tardis/textures/item/gui/chameleon/construct_button.png")
+                .button("Lock", FormImage.Type.URL, "https://github.com/eccentricdevotion/TARDIS-Resource-Pack/raw/master/assets/tardis/textures/item/gui/chameleon/lock_button.png")
                 .validResultHandler(response -> handleResponse(response))
                 .build();
         FloodgatePlayer player = FloodgateApi.getInstance().getPlayer(uuid);
@@ -69,7 +74,7 @@ public class FloodgateChameleonCircuitForm {
         HashMap<String, Object> wheref = new HashMap<>();
         wheref.put("tardis_id", id);
         wheref.put("type", Control.FRAME.getId());
-        ResultSetControls rsf = new ResultSetControls(plugin, wheref, true);
+        ResultSetControls rsf = new ResultSetControls(plugin, wheref, false);
         boolean hasFrame = rsf.resultSet();
         switch (label) {
             case "Apply" -> {
@@ -96,7 +101,7 @@ public class FloodgateChameleonCircuitForm {
                 }
                 TARDISMessage.send(player, "CHAM_SET", ChatColor.AQUA + "Factory Fresh");
             }
-            case "Adaptive Biome", "Adaptive Block"  -> {
+            case "Adaptive Biome", "Adaptive Block" -> {
                 PRESET adaptive = (preset.equals(PRESET.SUBMERGED)) ? PRESET.SUBMERGED : PRESET.FACTORY;
                 if (hasFrame) {
                     tcf.updateChameleonFrame(adaptive, rsf.getLocation());
@@ -130,11 +135,39 @@ public class FloodgateChameleonCircuitForm {
             }
             case "Shorted out" -> new FloodgateChameleonPresetForm(plugin, uuid).send();
             case "Construct" -> TARDISMessage.send(player, "CONSTRUCT_USE_SHELL");
+            case "Lock" -> {
+                // set the current adaptive preset as shorted out - this
+                // will allow locking in a usually unavailable biome preset
+                // ONLY if the Chameleon Circuit is set to Adaptive BIOME
+                if (isBiomeAdaptive(id)) {
+                    // get current location's biome
+                    HashMap<String, Object> wherel = new HashMap<>();
+                    wherel.put("tardis_id", id);
+                    ResultSetCurrentLocation rsl = new ResultSetCurrentLocation(plugin, wherel);
+                    if (rsl.resultSet()) {
+                        Location current = new Location(rsl.getWorld(), rsl.getX(), rsl.getY(), rsl.getZ());
+                        Biome biome = current.getBlock().getBiome();
+                        // get which preset
+                        PRESET which = getAdaption(biome);
+                        if (which != null) {
+                            set.put("adapti_on", 0);
+                            set.put("chameleon_preset", which.toString());
+                            if (hasFrame) {
+                                tcf.updateChameleonFrame(which, rsf.getLocation());
+                            }
+                            updateChameleonSign(rsc.getData(), which.toString(), player);
+                            TARDISMessage.send(player, "CHAM_SET", ChatColor.AQUA + which.getDisplayName());
+                        }
+                    }
+                } else {
+                    TARDISMessage.send(player, "CHAM_LOCK");
+                }
+            }
             default -> {
                 // do nothing
             }
         }
-        if (set.size() > 0) {
+        if (!set.isEmpty()) {
             plugin.getQueryFactory().doUpdate("tardis", set, wherec);
         }
     }
@@ -142,6 +175,21 @@ public class FloodgateChameleonCircuitForm {
     private void updateChameleonSign(ArrayList<HashMap<String, String>> map, String preset, Player player) {
         for (HashMap<String, String> entry : map) {
             TARDISStaticUtils.setSign(entry.get("location"), 3, preset, player);
+        }
+    }
+
+    private boolean isBiomeAdaptive(int id) {
+        HashMap<String, Object> where = new HashMap<>();
+        where.put("tardis_id", id);
+        ResultSetTardis rs = new ResultSetTardis(plugin, where, "", false, 0);
+        return rs.resultSet() && rs.getTardis().getAdaption() == Adaption.BIOME;
+    }
+
+    private PRESET getAdaption(Biome biome) {
+        try {
+            return PRESET.valueOf(plugin.getAdaptiveConfig().getString(biome.toString()));
+        } catch (IllegalArgumentException e) {
+            return null;
         }
     }
 }
