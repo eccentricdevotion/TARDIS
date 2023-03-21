16206680ad5f5c1e145ddecc5042d5026cfaeb11
eccentricdevotion <eccentricdevotion@gmail.com>
2023-03-21 22:02:14 +1300

    Get Blaster tachyon levels

--- src/main/java/me/eccentric_nz/tardissonicblaster/database/ResultSetBlaster.java
+++ src/main/java/me/eccentric_nz/tardissonicblaster/database/ResultSetBlaster.java
@@ -16,8 +16,13 @@
  */
 package me.eccentric_nz.tardissonicblaster.database;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.UUID;
 import me.eccentric_nz.TARDIS.TARDIS;
+import me.eccentric_nz.TARDIS.database.TARDISDatabaseConnection;
 
 /**
  *
@@ -25,16 +30,49 @@ import me.eccentric_nz.TARDIS.TARDIS;
  */
 public class ResultSetBlaster {
 
+    private final TARDISDatabaseConnection service = TARDISDatabaseConnection.getINSTANCE();
+    private final Connection connection = service.getConnection();
     private final TARDIS plugin;
+    private final String prefix;
     private final UUID uuid;
     private double tachyonLevel = 0;
 
     public ResultSetBlaster(TARDIS plugin, UUID uuid) {
         this.plugin = plugin;
         this.uuid = uuid;
+        prefix = this.plugin.getPrefix();
     }
 
     public boolean resultSet() {
+        PreparedStatement statement = null;
+        ResultSet rs = null;
+        String query = "SELECT * FROM " + prefix + "blaster WHERE uuid = ?";
+        try {
+            service.testConnection(connection);
+            statement = connection.prepareStatement(query);
+            statement.setString(1, uuid.toString());
+            rs = statement.executeQuery();
+            if (rs.isBeforeFirst()) {
+                rs.next();
+                tachyonLevel = rs.getDouble("tachyon_level");
+            } else {
+                return false;
+            }
+        } catch (SQLException e) {
+            plugin.debug("ResultSet error for blaster table! " + e.getMessage());
+            return false;
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+                if (statement != null) {
+                    statement.close();
+                }
+            } catch (SQLException e) {
+                plugin.debug("Error closing blaster table! " + e.getMessage());
+            }
+        }
         return true;
     }
 
