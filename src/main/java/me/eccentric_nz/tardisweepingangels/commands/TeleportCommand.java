b9a852c5a444c4753a5eb7056c42ab48aec8831f
eccentricdevotion <eccentricdevotion@gmail.com>
2023-07-26 20:39:32 +1200

    Update TeleportCommand.java

--- src/main/java/me/eccentric_nz/tardisweepingangels/commands/TeleportCommand.java
+++ src/main/java/me/eccentric_nz/tardisweepingangels/commands/TeleportCommand.java
@@ -1,16 +1,14 @@
 package me.eccentric_nz.tardisweepingangels.commands;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
-
+import me.eccentric_nz.TARDIS.TARDIS;
+import me.eccentric_nz.TARDIS.enumeration.TardisModule;
 import org.bukkit.Location;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import me.eccentric_nz.TARDIS.TARDIS;
-import me.eccentric_nz.TARDIS.enumeration.TardisModule;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
 public class TeleportCommand {
 
@@ -22,17 +20,16 @@ public class TeleportCommand {
 
     public boolean add(CommandSender sender, String[] args) {
         String message = "Added a weeping angels' teleport location successfully.";
-        String a = args[1].toLowerCase();
+        String a = (args.length > 1) ? args[1].toLowerCase() : "";
         if (a.equals("true") || a.equals("false")) {
-            message = "Set 'angels.teleport_to_location' to " + args[1];
+            message = "Set 'angels.teleport_to_location' to " + a;
             plugin.getMonstersConfig().set("angels.teleport_to_location", Boolean.valueOf(a));
         } else if (sender instanceof Player player) {
             Location location = player.getLocation();
-            String tpLoc = location.getWorld().getName() + "," + location.getBlockX() + "," + location.getBlockY() + ","
-                    + location.getBlockZ();
+            String tpLoc = location.getWorld().getName() + "," + location.getBlockX() + "," + location.getBlockY() + "," + location.getBlockZ();
             List<String> list;
             if (a.equals("replace")) {
-                list = Arrays.asList(tpLoc);
+                list = List.of(tpLoc);
             } else {
                 list = plugin.getMonstersConfig().getStringList("angels.teleport_locations");
                 list.add(tpLoc);
@@ -42,8 +39,7 @@ public class TeleportCommand {
         try {
             String monstersPath = plugin.getDataFolder() + File.separator + "monsters.yml";
             plugin.getMonstersConfig().save(new File(monstersPath));
-            plugin.getMessenger().message(sender, TardisModule.MONSTERS,
-                    message);
+            plugin.getMessenger().message(sender, TardisModule.MONSTERS, message);
         } catch (IOException io) {
             plugin.debug("Could not save monsters.yml, " + io.getMessage());
         }
