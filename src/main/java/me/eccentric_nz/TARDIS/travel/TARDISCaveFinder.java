73429a03fceee2a34006e774934c174c2e0eea18
Eccentric Devotion <eccentricdevotion@gmail.com>
2023-08-05 10:30:05 +1200

    Fix cave finder valid landing blocks
    Should make cave finding faster again...

--- src/main/java/me/eccentric_nz/TARDIS/travel/TARDISCaveFinder.java
+++ src/main/java/me/eccentric_nz/TARDIS/travel/TARDISCaveFinder.java
@@ -16,10 +16,6 @@
  */
 package me.eccentric_nz.TARDIS.travel;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
 import me.eccentric_nz.TARDIS.TARDIS;
 import me.eccentric_nz.TARDIS.database.resultset.ResultSetCurrentLocation;
 import me.eccentric_nz.TARDIS.enumeration.COMPASS;
@@ -29,6 +25,11 @@ import org.bukkit.Tag;
 import org.bukkit.World;
 import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * @author eccentric_nz
@@ -133,7 +134,7 @@ public class TARDISCaveFinder {
             if (w.getBlockAt(x, y, z).getType().isAir()) {
                 int yy = getLowestAirBlock(w, x, y, z);
                 // check there is enough height for the police box
-                if (yy <= y - 3 && !Tag.BASE_STONE_OVERWORLD.isTagged(w.getBlockAt(x - 1, yy - 1, z - 1).getType())) {
+                if (yy <= y - 3 && Tag.BASE_STONE_OVERWORLD.isTagged(w.getBlockAt(x - 1, yy - 1, z - 1).getType())) {
                     // check there is room for the police box
                     if (w.getBlockAt(x - 1, yy, z - 1).getType().isAir() && w.getBlockAt(x - 1, yy, z).getType().isAir() && w.getBlockAt(x - 1, yy, z + 1).getType().isAir() && w.getBlockAt(x, yy, z - 1).getType().isAir() && w.getBlockAt(x, yy, z + 1).getType().isAir() && w.getBlockAt(x + 1, yy, z - 1).getType().isAir() && w.getBlockAt(x + 1, yy, z).getType().isAir() && w.getBlockAt(x + 1, yy, z + 1).getType().isAir()) {
                         // finally check there is space to exit the police box
