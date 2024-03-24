c4813e724b98183f4405e7d8192107c07261b651
eccentricdevotion <eccentricdevotion@gmail.com>
2024-03-24 19:57:21 +1300

    Only sherds

--- src/main/java/me/eccentric_nz/TARDIS/doors/TARDISCustomDoorLoader.java
+++ src/main/java/me/eccentric_nz/TARDIS/doors/TARDISCustomDoorLoader.java
@@ -3,32 +3,11 @@ package me.eccentric_nz.TARDIS.doors;
 import me.eccentric_nz.TARDIS.TARDIS;
 import me.eccentric_nz.TARDIS.utility.TARDISNumberParsers;
 import org.bukkit.Material;
+import org.bukkit.Tag;
 
 public class TARDISCustomDoorLoader {
 
     private final TARDIS plugin;
-    /*
-    ANGLER_POTTERY_SHERD,
-    ARCHER_POTTERY_SHERD,
-    ARMS_UP_POTTERY_SHERD,
-    BLADE_POTTERY_SHERD,
-    BREWER_POTTERY_SHERD,
-    BURN_POTTERY_SHERD,
-    DANGER_POTTERY_SHERD,
-    EXPLORER_POTTERY_SHERD,
-    FRIEND_POTTERY_SHERD,
-    HEART_POTTERY_SHERD,
-    HEARTBREAK_POTTERY_SHERD,
-    HOWL_POTTERY_SHERD,
-    MINER_POTTERY_SHERD,
-    MOURNER_POTTERY_SHERD,
-    PLENTY_POTTERY_SHERD,
-    PRIZE_POTTERY_SHERD,
-    SHEAF_POTTERY_SHERD,
-    SHELTER_POTTERY_SHERD,
-    SKULL_POTTERY_SHERD,
-    SNORT_POTTERY_SHERD
-     */
 
     public TARDISCustomDoorLoader(TARDIS plugin) {
         this.plugin = plugin;
@@ -44,19 +23,14 @@ public class TARDISCustomDoorLoader {
         for (String r : plugin.getCustomDoorsConfig().getKeys(false)) {
             try {
                 Material material = Material.valueOf(plugin.getCustomDoorsConfig().getString(r + ".material"));
-                int[] frames = getFrames(plugin.getCustomDoorsConfig().getString(r + ".animation_sequence", "0,1"));
-                Door door = new Door(
-                        r,
-                        material,
-                        frames,
-                        plugin.getCustomDoorsConfig().getInt(r + ".frame_rate"),
-                        plugin.getCustomDoorsConfig().getBoolean(r + ".extra"),
-                        plugin.getCustomDoorsConfig().getString(r + ".open_sound"),
-                        plugin.getCustomDoorsConfig().getString(r + ".close_sound"),
-                        true
-                );
-                Door.byMaterial.put(material, door);
-                Door.byName.put("DOOR_" + r.toUpperCase(), door);
+                if (Tag.ITEMS_DECORATED_POT_SHERDS.isTagged(material)) {
+                    int[] frames = getFrames(plugin.getCustomDoorsConfig().getString(r + ".animation_sequence", "0,1"));
+                    Door door = new Door(r, material, frames, plugin.getCustomDoorsConfig().getInt(r + ".frame_rate"), plugin.getCustomDoorsConfig().getBoolean(r + ".extra"), plugin.getCustomDoorsConfig().getString(r + ".open_sound"), plugin.getCustomDoorsConfig().getString(r + ".close_sound"), true);
+                    Door.byMaterial.put(material, door);
+                    Door.byName.put("DOOR_" + r.toUpperCase(), door);
+                } else {
+                    plugin.getMessenger().message(plugin.getConsole(), "Custom door [" + r + "] material must be a pottery sherd!");
+                }
             } catch (IllegalArgumentException e) {
                 plugin.debug("Invalid custom door item material for " + r + "!");
             }
