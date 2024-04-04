3b726b1a6e8ce9f8ee6a25fb8bb0eb469d18a243
eccentricdevotion <eccentricdevotion@gmail.com>
2024-04-04 23:00:26 +1300

    Update RandomiserInteraction.java

--- src/main/java/me/eccentric_nz/TARDIS/console/interaction/RandomiserInteraction.java
+++ src/main/java/me/eccentric_nz/TARDIS/console/interaction/RandomiserInteraction.java
@@ -1,6 +1,15 @@
 package me.eccentric_nz.TARDIS.console.interaction;
 
 import me.eccentric_nz.TARDIS.TARDIS;
+import me.eccentric_nz.TARDIS.advanced.TARDISCircuitChecker;
+import me.eccentric_nz.TARDIS.control.TARDISRandomButton;
+import me.eccentric_nz.TARDIS.database.data.Tardis;
+import me.eccentric_nz.TARDIS.database.resultset.ResultSetTardis;
+import me.eccentric_nz.TARDIS.enumeration.Difficulty;
+import me.eccentric_nz.TARDIS.enumeration.TardisModule;
+import org.bukkit.entity.Player;
+
+import java.util.HashMap;
 
 public class RandomiserInteraction {
 
@@ -10,7 +19,29 @@ public class RandomiserInteraction {
         this.plugin = plugin;
     }
 
-    public void generateDestination() {
-
+    public void generateDestination(int id, Player player) {
+        if (plugin.getTrackerKeeper().getInSiegeMode().contains(id)) {
+            plugin.getMessenger().send(player, TardisModule.TARDIS, "SIEGE_NO_CONTROL");
+            return;
+        }
+        // get circuit checker
+        TARDISCircuitChecker tcc = null;
+        if (!plugin.getDifficulty().equals(Difficulty.EASY)) {
+            tcc = new TARDISCircuitChecker(plugin, id);
+            tcc.getCircuits();
+        }
+        if (tcc != null && !tcc.hasInput() && !plugin.getUtils().inGracePeriod(player, false)) {
+            plugin.getMessenger().send(player, TardisModule.TARDIS, "INPUT_MISSING");
+            return;
+        }
+        // get tardis record
+        HashMap<String, Object> where = new HashMap<>();
+        where.put("tardis_id", id);
+        ResultSetTardis rs = new ResultSetTardis(plugin, where, "", false, 0);
+        if (!rs.resultSet()) {
+            return;
+        }
+        Tardis tardis = rs.getTardis();
+        new TARDISRandomButton(plugin, player, id, tardis.getArtronLevel(), 0, tardis.getCompanions(), tardis.getUuid()).clickButton();
     }
 }
