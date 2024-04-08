b7dba1884bccd6177ace9ccbc14a4f2ef3e59861
Eccentric Devotion <eccentricdevotion@gmail.com>
2024-04-08 12:28:11 +1200

    Bukkit.getLogger() should only be used by Minecraft

--- src/main/java/me/eccentric_nz/TARDIS/utility/RecipeChecker.java
+++ src/main/java/me/eccentric_nz/TARDIS/utility/RecipeChecker.java
@@ -2,6 +2,7 @@ package me.eccentric_nz.TARDIS.utility;
 
 import it.unimi.dsi.fastutil.objects.Object2ObjectLinkedOpenHashMap;
 import it.unimi.dsi.fastutil.objects.Object2ObjectMap;
+import me.eccentric_nz.TARDIS.TARDIS;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerLevel;
@@ -30,13 +31,13 @@ public class RecipeChecker implements Runnable {
                         String key = r.getKey().getNamespace().toLowerCase();
                         String path = r.getKey().getPath();
                         if (key.startsWith("tardis")) {
-                            Bukkit.getLogger().log(Level.INFO, path + " => " + shaped.getResultItem(world.registryAccess()).toString());
+                            TARDIS.plugin.getLogger().log(Level.INFO, path + " => " + shaped.getResultItem(world.registryAccess()).toString());
                         }
                     }
                 }
             }
         } catch (IllegalArgumentException e) {
-            Bukkit.getLogger().log(Level.WARNING, "Skipping recipe...");
+            TARDIS.plugin.getLogger().log(Level.WARNING, "Skipping recipe...");
         }
     }
 }
