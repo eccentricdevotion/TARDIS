75d1518347785e320f9cdf4564d762a4a4a4483d
eccentricdevotion <eccentricdevotion@gmail.com>
2024-05-04 21:33:50 +1200

    Require 1.20.6

--- src/main/java/me/eccentric_nz/TARDIS/TARDIS.java
+++ src/main/java/me/eccentric_nz/TARDIS/TARDIS.java
@@ -140,7 +140,7 @@ public class TARDIS extends JavaPlugin {
     private final HashMap<String, String> versions = new HashMap<>();
     private final String versionRegex = "(\\d+[.])+\\d+";
     private final Pattern versionPattern = Pattern.compile(versionRegex);
-    private final String serverStr = "1.20.5";
+    private final String serverStr = "1.20.6";
     private TARDISChatGUI jsonKeeper;
     private TARDISUpdateChatGUI updateChatGUI;
     //    public TARDISFurnaceRecipe fornacis;
