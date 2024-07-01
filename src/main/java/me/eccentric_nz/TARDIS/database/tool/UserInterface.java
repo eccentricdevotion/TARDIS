2e8e4d78620284d84445aa5ad5e24e6586e9100c
eccentricdevotion <eccentricdevotion@gmail.com>
2024-07-01 14:19:36 +1200

    Use FlatLafDarcula theme for the database conversion GUI

--- src/main/java/me/eccentric_nz/TARDIS/database/tool/UserInterface.java
+++ src/main/java/me/eccentric_nz/TARDIS/database/tool/UserInterface.java
@@ -16,11 +16,13 @@
  */
 package me.eccentric_nz.TARDIS.database.tool;
 
+import com.formdev.flatlaf.FlatDarculaLaf;
+
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.io.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.swing.*;
-import javax.swing.filechooser.FileNameExtensionFilter;
 
 /**
  * @author eccentric_nz
@@ -64,27 +66,9 @@ public class UserInterface extends javax.swing.JFrame {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        /*
-         * Set the Nimbus look and feel
-         */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /*
-         * If Nimbus (introduced in Java SE 6) is not available, stay with the
-         * default look and feel. For details see
-         * http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException | UnsupportedLookAndFeelException | IllegalAccessException | InstantiationException ex) {
-            java.util.logging.Logger.getLogger(UserInterface.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
 
+        // Use FlatLaf Darcula theme for the GUI
+        FlatDarculaLaf.setup();
         /*
          * Create and display the form
          */
---
+++ src/main/java/me/eccentric_nz/TARDIS/rooms/eye/EyeOfHarmonyAction.java
@@ -0,0 +1,17 @@
+package me.eccentric_nz.TARDIS.rooms.eye;
+
+import me.eccentric_nz.TARDIS.TARDIS;
+import org.bukkit.entity.Player;
+
+public class EyeOfHarmonyAction {
+
+    private final TARDIS plugin;
+
+    public EyeOfHarmonyAction(TARDIS plugin) {
+        this.plugin = plugin;
+    }
+
+    public void openGUI(int id, Player player) {
+        
+    }
+}
