b80a7ef4c17696856aaa78822220ae69683fbdee
Eccentric Devotion <eccentricdevotion@gmail.com>
2024-11-06 10:25:04 +1300

    Fic modelled console buttons getting desynced by double-clicks

--- Core/src/main/java/me/eccentric_nz/TARDIS/console/models/ButtonModel.java
+++ Core/src/main/java/me/eccentric_nz/TARDIS/console/models/ButtonModel.java
@@ -16,14 +16,23 @@ public class ButtonModel {
         ItemStack is = display.getItemStack();
         ItemMeta im = is.getItemMeta();
         int cmd = im.getCustomModelData();
-        im.setCustomModelData(cmd + 1000);
-        is.setItemMeta(im);
-        display.setItemStack(is);
-        plugin.getServer().getScheduler().scheduleSyncDelayedTask(plugin, () -> {
-            display.getWorld().playSound(display, Sound.BLOCK_BAMBOO_WOOD_BUTTON_CLICK_OFF, 1, 1);
-            im.setCustomModelData(cmd);
+        if (cmd < 2000) {
+            im.setCustomModelData(cmd + 1000);
             is.setItemMeta(im);
             display.setItemStack(is);
-        }, 10);
+            plugin.getServer().getScheduler().scheduleSyncDelayedTask(plugin, () -> {
+                display.getWorld().playSound(display, Sound.BLOCK_BAMBOO_WOOD_BUTTON_CLICK_OFF, 1, 1);
+                im.setCustomModelData(cmd);
+                is.setItemMeta(im);
+                display.setItemStack(is);
+            }, 10);
+        } else {
+            plugin.getServer().getScheduler().scheduleSyncDelayedTask(plugin, () -> {
+                display.getWorld().playSound(display, Sound.BLOCK_BAMBOO_WOOD_BUTTON_CLICK_OFF, 1, 1);
+                im.setCustomModelData(cmd - 1000);
+                is.setItemMeta(im);
+                display.setItemStack(is);
+            }, 10);
+        }
     }
 }
