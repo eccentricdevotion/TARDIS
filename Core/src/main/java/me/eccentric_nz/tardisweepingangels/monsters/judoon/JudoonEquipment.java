f7e9f2ae16b0c474a3c566823fa1d881315487aa
Eccentric Devotion <eccentricdevotion@gmail.com>
2025-01-12 16:15:49 +1300

    Make Judoon slightly bigger

--- Core/src/main/java/me/eccentric_nz/tardisweepingangels/monsters/judoon/JudoonEquipment.java
+++ Core/src/main/java/me/eccentric_nz/tardisweepingangels/monsters/judoon/JudoonEquipment.java
@@ -22,6 +22,7 @@ import me.eccentric_nz.tardisweepingangels.equip.FollowerEquipper;
 import me.eccentric_nz.tardisweepingangels.utils.Monster;
 import org.bukkit.Material;
 import org.bukkit.OfflinePlayer;
+import org.bukkit.attribute.Attribute;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -40,6 +41,8 @@ public class JudoonEquipment {
             hand.setItemMeta(tim);
             entity.getEquipment().setItemInMainHand(hand);
             new FollowerEquipper().setOptionsAndInvisibilty(player, entity, Monster.JUDOON);
+            // set entity scale
+            entity.getAttribute(Attribute.SCALE).setBaseValue(1.25d);
         } else {
             PotionEffect potionEffect = new PotionEffect(PotionEffectType.INVISIBILITY, Integer.MAX_VALUE, 1, true, false);
             entity.addPotionEffect(potionEffect);
