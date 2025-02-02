7f544bb2d64b6416e4593509e1e52a906f44b609
Eccentric Devotion <eccentricdevotion@gmail.com>
2025-02-02 19:58:33 +1300

    Update copyright year to 2025

--- Core/src/main/java/me/eccentric_nz/TARDIS/blueprints/BlueprintWeepingAngels.java
+++ Core/src/main/java/me/eccentric_nz/TARDIS/blueprints/BlueprintWeepingAngels.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2024 eccentric_nz
+ * Copyright (C) 2025 eccentric_nz
  *
  * This program is free software"), you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
