1ce591eb133b45a462fb98db1e567625b70b05b6
Eccentric Devotion <eccentricdevotion@gmail.com>
2025-12-19 22:39:09 +1300

    Revert Java version until Minecraft requires it in next release

--- build.gradle.kts
+++ build.gradle.kts
@@ -179,7 +179,7 @@ allprojects {
 tasks {
     compileJava {
         // Set the release flag
-        options.release = 25
+        options.release = 21
     }
     javadoc {
         options.encoding = Charsets.UTF_8.name() // We want UTF-8 for everything
