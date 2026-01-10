6a820d4d7b6e1c85d64fa9fd29b8d00436204459
Eccentric Devotion <eccentricdevotion@gmail.com>
2026-01-10 19:39:32 +1300

    Update ControlListener.java

--- src/main/java/me/eccentric_nz/TARDIS/control/ControlListener.java
+++ src/main/java/me/eccentric_nz/TARDIS/control/ControlListener.java
@@ -235,7 +235,11 @@ public class ControlListener implements Listener {
                                 }
                                 case 55 -> new TelevisionAction(plugin).openGUI(player);
                                 case 58 -> new HappyGhastRelease(plugin).undock(block, id, player);
-                                case 59 -> new ArchitecturalReconfiguration(plugin).open(player);
+                                case 59 -> {
+                                    if (plugin.getConfig().getBoolean("modules.blueprints")) {
+                                        new ArchitecturalReconfiguration(plugin).open(player);
+                                    }
+                                }
                                 default -> { }
                             }
                         } else if (action.equals(Action.PHYSICAL)) {
