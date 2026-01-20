1902b9fdc7a34e183000c4f7f85fefdf8109b9d7
Rate <eccentricdevotion@gmail.com>
2026-01-20 16:20:54 +1300

    Air's formula

--- src/main/java/me/eccentric_nz/TARDIS/commands/dev/lists/ConsoleCostLister.java
+++ src/main/java/me/eccentric_nz/TARDIS/commands/dev/lists/ConsoleCostLister.java
@@ -2,14 +2,18 @@ package me.eccentric_nz.TARDIS.commands.dev.lists;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+import com.mojang.datafixers.util.Pair;
 import me.eccentric_nz.TARDIS.TARDIS;
+import me.eccentric_nz.TARDIS.enumeration.ConsoleSize;
 import me.eccentric_nz.TARDIS.enumeration.Desktops;
 import me.eccentric_nz.TARDIS.enumeration.TardisModule;
 import me.eccentric_nz.TARDIS.schematic.SchematicGZip;
 import me.eccentric_nz.TARDIS.utility.TARDISStringUtils;
 import org.bukkit.block.data.BlockData;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class ConsoleCostLister {
@@ -59,6 +63,12 @@ public class ConsoleCostLister {
             }
             consoleBlockCounts.put(fileName, blockTypes);
         }
+        Pair<String, Integer> small = new Pair<>("small", 0);
+        Pair<String, Integer> medium = new Pair<>("medium", 0);
+        Pair<String, Integer> tall = new Pair<>("tall", 0);
+        Pair<String, Integer> wide = new Pair<>("wide", 0);
+        Pair<String, Integer> massive = new Pair<>("massive", 0);
+        List<CostData> calculated = new ArrayList<>();
         for (Map.Entry<String, HashMap<String, Integer>> c : consoleBlockCounts.entrySet()) {
             int cost = 0;
             for (Map.Entry<String, Integer> entry : c.getValue().entrySet()) {
@@ -77,8 +87,53 @@ public class ConsoleCostLister {
             float tmp_cost = artron_cost / 10.0f;
             int config_cost = (((int) tmp_cost) / 25) * 25;
             int current_cost = plugin.getArtronConfig().getInt("upgrades." + c.getKey());
-            plugin.debug("| " + TARDISStringUtils.capitalise(c.getKey()) + " | " + artron_cost + " | " + config_cost + " |" + current_cost + " |");
+            ConsoleSize size = Desktops.getBY_PERMS().get(c.getKey()).getConsoleSize();
+            switch (size) {
+                case SMALL -> {
+                    if (artron_cost > small.getSecond()) {
+                        small = new Pair<>(c.getKey(), artron_cost);
+                    }
+                }
+                case MEDIUM -> {
+                    if (artron_cost > medium.getSecond()) {
+                        medium = new Pair<>(c.getKey(), artron_cost);
+                    }
+                }
+                case TALL -> {
+                    if (artron_cost > tall.getSecond()) {
+                        tall = new Pair<>(c.getKey(), artron_cost);
+                    }
+                }
+                case WIDE -> {
+                    if (artron_cost > wide.getSecond()) {
+                        wide = new Pair<>(c.getKey(), artron_cost);
+                    }
+                }
+                default -> {
+                    if (artron_cost > massive.getSecond()) {
+                        massive = new Pair<>(c.getKey(), artron_cost);
+                    }
+                }
+            }
+            calculated.add(new CostData(TARDISStringUtils.capitalise(c.getKey()), artron_cost, config_cost, current_cost, size));
         }
+        for (CostData data : calculated) {
+            // airomis formula
+            int formula_cost = switch (data.size()) {
+                case SMALL -> (int) (((float) data.artron() / (float) small.getSecond()) * 5000);
+                case MEDIUM -> (int) (((float) data.artron() / (float) medium.getSecond()) * 10000);
+                case TALL -> (int) (((float) data.artron() / (float) tall.getSecond()) * 15000);
+                case WIDE -> (int) (((float) data.artron() / (float) wide.getSecond()) * 20000);
+                default -> (int) (((float) data.artron() / (float) massive.getSecond()) * 25000);
+            };
+            plugin.debug("| " + data.name() + " | " + data.artron() + " | " + data.updated() + " | " + data.current() + " | " + formula_cost + " | " + data.size() + " |");
+        }
+        plugin.debug("Most expensive:");
+        plugin.debug("SMALL " + small.getFirst() + ", " + small.getSecond());
+        plugin.debug("MEDIUM " + medium.getFirst() + ", " + medium.getSecond());
+        plugin.debug("TALL " + tall.getFirst() + ", " + tall.getSecond());
+        plugin.debug("WIDE " + wide.getFirst() + ", " + wide.getSecond());
+        plugin.debug("MASSIVE " + massive.getFirst() + ", " + massive.getSecond());
     }
 
     private String getMaterialAsString(String data) {
---
+++ src/main/java/me/eccentric_nz/TARDIS/commands/dev/lists/CostData.java
@@ -0,0 +1,6 @@
+package me.eccentric_nz.TARDIS.commands.dev.lists;
+
+import me.eccentric_nz.TARDIS.enumeration.ConsoleSize;
+
+public record CostData(String name, int artron, int updated, int current, ConsoleSize size) {
+}
