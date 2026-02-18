72fd12383811f101c37d037be9d2aae7c14e2b47
Rate <eccentricdevotion@gmail.com>
2026-02-19 08:57:17 +1300

    Brigadier Call command

---
+++ src/main/java/me/eccentric_nz/TARDIS/brigadier/CallCommandNode.java
@@ -0,0 +1,39 @@
+package me.eccentric_nz.TARDIS.brigadier;
+
+import com.mojang.brigadier.Command;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.tree.LiteralCommandNode;
+import io.papermc.paper.command.brigadier.CommandSourceStack;
+import io.papermc.paper.command.brigadier.Commands;
+import io.papermc.paper.command.brigadier.argument.ArgumentTypes;
+import io.papermc.paper.command.brigadier.argument.resolvers.selector.PlayerSelectorArgumentResolver;
+import me.eccentric_nz.TARDIS.TARDIS;
+import me.eccentric_nz.TARDIS.brigadier.arguments.BookArgumentType;
+import me.eccentric_nz.TARDIS.commands.TARDISCallRequestCommand;
+import me.eccentric_nz.TARDIS.commands.TARDISCommandHelper;
+import me.eccentric_nz.TARDIS.commands.book.BookUtility;
+import me.eccentric_nz.TARDIS.commands.book.TARDISBook;
+import org.bukkit.entity.Player;
+
+public class CallCommandNode {
+
+    private final TARDIS plugin;
+
+    public CallCommandNode(TARDIS plugin) {
+        this.plugin = plugin;
+    }
+
+    LiteralCommandNode<CommandSourceStack> build() {
+        LiteralArgumentBuilder<CommandSourceStack> command = Commands.literal("tardiscall")
+                .requires(ctx -> ctx.getExecutor() instanceof Player)
+                .then(Commands.argument("player", ArgumentTypes.player())
+                        .executes(ctx -> {
+                            Player player = (Player) ctx.getSource().getExecutor();
+                            PlayerSelectorArgumentResolver targetResolver = ctx.getArgument("player", PlayerSelectorArgumentResolver.class);
+                            Player requested = targetResolver.resolve(ctx.getSource()).getFirst();
+                            new TARDISCallRequestCommand(plugin).requestComeHere(player, requested);
+                            return Command.SINGLE_SUCCESS;
+                        }));
+        return command.build();
+    }
+}
