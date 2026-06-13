f1ba835959ba894de725944e88eb3db67a8112e1
Eccentric Devotion <eccentricdevotion@gmail.com>
2026-06-13 13:41:29 +1200

    Update version to 7.0.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 
 group = "me.eccentric_nz"
 val buildNumber = "-b${System.getenv("BUILD_NUMBER") ?: (System.getenv("SHORT_SHA") ?: ".local")}"
-version = "7.0.2${buildNumber}"
+version = "7.0.3${buildNumber}"
 
 repositories {
     mavenCentral()
