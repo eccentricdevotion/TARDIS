25e551672be1a61bf5e3ef2191af49c757c519cb
eccentricdevotion <eccentricdevotion@gmail.com>
2026-08-06 15:43:09 +1200

    Tidy up permissions suggestions

--- src/main/java/me/eccentric_nz/TARDIS/commands/admin/BlueprintCommand.java
+++ src/main/java/me/eccentric_nz/TARDIS/commands/admin/BlueprintCommand.java
@@ -14,10 +14,18 @@ public class BlueprintCommand {
     }
 
     public void hasPermission(CommandSender sender, Player player, String perm) {
-        if (TARDISPermission.hasPermission(player, perm)) {
-            plugin.getMessenger().message(sender, TardisModule.TARDIS, player.getName() + "has " + perm);
-        } else {
-            plugin.getMessenger().message(sender, TardisModule.TARDIS, player.getName() + "does NOT have " + perm);
+        if (!isPermission(perm)) {
+            plugin.getMessenger().sendWithColours(sender, TardisModule.TARDIS, perm, "#aaff33", " is not a valid TARDIS permission node! ", "#ffffff");
+            return;
         }
+        if (TARDISPermission.hasPermission(player, perm)) {
+            plugin.getMessenger().sendWithColours(sender, TardisModule.TARDIS, player.getName() + " has ", "#ffffff", perm, "#aaff33");
+        } else {
+            plugin.getMessenger().sendWithColours(sender, TardisModule.TARDIS, player.getName() + " does NOT have ", "#ffffff", perm, "#aaff33");
+        }
+    }
+
+    private boolean isPermission(String perm) {
+        return plugin.getGeneralKeeper().getPluginYAML().getConfigurationSection("permissions").getKeys(true).contains(perm);
     }
 }
