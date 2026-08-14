5fef6ea442c3017a31f2b177311cedb0eb1a465c
Eccentric Devotion <eccentricdevotion@gmail.com>
2026-08-15 08:03:00 +1200

    Add to balloons to mannequin dev command

--- src/main/java/me/eccentric_nz/TARDIS/commands/dev/MannequinCommand.java
+++ src/main/java/me/eccentric_nz/TARDIS/commands/dev/MannequinCommand.java
@@ -6,6 +6,8 @@ import me.eccentric_nz.TARDIS.TARDIS;
 import me.eccentric_nz.TARDIS.TARDISConstants;
 import me.eccentric_nz.TARDIS.skins.MannequinSkins;
 import me.eccentric_nz.TARDIS.skins.Skin;
+import me.eccentric_nz.tardischemistry.product.Product;
+import me.eccentric_nz.tardischemistry.product.ProductBuilder;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.EntityType;
@@ -15,6 +17,7 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.MainHand;
 import org.bukkit.persistence.PersistentDataType;
 
+import java.util.List;
 import java.util.UUID;
 
 public class MannequinCommand {
@@ -35,6 +38,18 @@ public class MannequinCommand {
         if (which.equals("roman") && TARDISConstants.RANDOM.nextBoolean()) {
             mannequin.setMainHand(TARDISConstants.RANDOM.nextBoolean() ? MainHand.LEFT : MainHand.RIGHT);
             mannequin.getEquipment().setItemInMainHand(ItemStack.of(TARDISConstants.RANDOM.nextBoolean() ? Material.IRON_SWORD : Material.IRON_SPEAR));
+        }
+        if (which.equals("balloon")) {
+            mannequin.setMainHand(TARDISConstants.RANDOM.nextBoolean() ? MainHand.LEFT : MainHand.RIGHT);
+            // make a balloon
+            List<Product> balloons = List.of(
+                    Product.White_Balloon, Product.Orange_Balloon, Product.Magenta_Balloon, Product.Light_Blue_Balloon,
+                    Product.Yellow_Balloon, Product.Lime_Balloon, Product.Pink_Balloon, Product.Gray_Balloon,
+                    Product.Light_Gray_Balloon, Product.Cyan_Balloon, Product.Purple_Balloon, Product.Blue_Balloon,
+                    Product.Brown_Balloon, Product.Green_Balloon, Product.Red_Balloon, Product.Black_Balloon
+            );
+            ItemStack balloon = ProductBuilder.getProduct(balloons.get(TARDISConstants.RANDOM.nextInt(16)));
+            mannequin.getEquipment().setItemInMainHand(balloon);
         }
     }
 }
