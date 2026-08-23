23c4ce2acf1fd08b635cb233e6b1b10cc7efcda0
Eccentric Devotion <eccentricdevotion@gmail.com>
2026-08-23 19:08:08 +1200

    Fix `/ttravel structure` command

--- src/main/java/me/eccentric_nz/TARDIS/brigadier/TravelCommandNode.java
+++ src/main/java/me/eccentric_nz/TARDIS/brigadier/TravelCommandNode.java
@@ -219,7 +219,8 @@ public class TravelCommandNode {
                                     Player player = (Player) ctx.getSource().getSender();
                                     int id = TravelUtilities.getId(plugin, player);
                                     if (id > 0) {
-                                        Structure type = ctx.getArgument("type", Structure.class);
+                                        TypedKey<Structure> key = RegistryArgumentExtractor.getTypedKey(ctx, RegistryKey.STRUCTURE, "type");
+                                        Structure type = RegistryAccess.registryAccess().getRegistry(key.registryKey()).get(key.key());
                                         StructureUtilities.search(plugin, player, type, id);
                                     }
                                     return Command.SINGLE_SUCCESS;
@@ -250,7 +251,8 @@ public class TravelCommandNode {
                                     Player player = (Player) ctx.getSource().getSender();
                                     int id = TravelUtilities.getId(plugin, player);
                                     if (id > 0) {
-                                        Structure type = ctx.getArgument("type", Structure.class);
+                                        TypedKey<Structure> key = RegistryArgumentExtractor.getTypedKey(ctx, RegistryKey.STRUCTURE, "type");
+                                        Structure type = RegistryAccess.registryAccess().getRegistry(key.registryKey()).get(key.key());
                                         StructureUtilities.search(plugin, player, type, id);
                                     }
                                     return Command.SINGLE_SUCCESS;
