8852a4e482a0b6e5b65c584bd68cbec011fd30fb
eccentricdevotion <eccentricdevotion@gmail.com>
2026-09-16 12:52:15 +1200

    Fix datapack

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 
 group = "me.eccentric_nz"
 val buildNumber = "-b${System.getenv("BUILD_NUMBER") ?: (System.getenv("SHORT_SHA") ?: ".local")}"
-version = "7.0.4${buildNumber}"
+version = "7.1.0${buildNumber}"
 
 repositories {
     mavenCentral()
