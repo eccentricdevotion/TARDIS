b06705fe904241036e09bd3e3e46765f05431577
Eccentric Devotion <eccentricdevotion@gmail.com>
2026-09-17 19:02:46 +1200

    Check player has perm for chunk loading

--- src/main/java/me/eccentric_nz/TARDIS/control/actions/ChunkLoaderAction.java
+++ src/main/java/me/eccentric_nz/TARDIS/control/actions/ChunkLoaderAction.java
@@ -1,6 +1,7 @@
 package me.eccentric_nz.TARDIS.control.actions;
 
 import me.eccentric_nz.TARDIS.TARDIS;
+import me.eccentric_nz.TARDIS.blueprints.TARDISPermission;
 import me.eccentric_nz.TARDIS.enumeration.TardisModule;
 import me.eccentric_nz.TARDIS.rooms.loader.ChunkLoaderInventory;
 import org.bukkit.entity.Player;
@@ -14,6 +15,10 @@ public class ChunkLoaderAction {
     }
 
     public void openGUI(Player player) {
+        if (!TARDISPermission.hasPermission(player, "tardis.chunk_tickets")) {
+            plugin.getMessenger().send(player, TardisModule.TARDIS, "NO_PERMS");
+            return;
+        }
         if (plugin.getConfig().getBoolean("allow.chunk_tickets")) {
             player.openInventory(new ChunkLoaderInventory(plugin).getInventory());
         } else {
