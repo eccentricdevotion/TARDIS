80526b0f80106886da43b3da8f5336e583f780f3
Eccentric Devotion <eccentricdevotion@gmail.com>
2026-09-20 12:47:54 +1200

    Update paperweight.userdev

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import java.io.ByteArrayOutputStream
 
 plugins {
     `java-library`
-    id("io.papermc.paperweight.userdev") version "2.0.0-beta.21"
+    id("io.papermc.paperweight.userdev") version "2.0.0-beta.23"
     id("com.gradleup.shadow") version "9.6.1"
     id("java")
     id("io.papermc.hangar-publish-plugin") version "0.1.4"
