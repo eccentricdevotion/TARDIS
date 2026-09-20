1f78508fafc567ccf9d68d0260f08d5fc46e8b9f
Eccentric Devotion <eccentricdevotion@gmail.com>
2026-09-20 12:44:29 +1200

    Squashed commit of the following:
    commit 12dfc5c3d6b5d17662e2ba799a83a296a0089d30
    Author: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Date:   Sat Sep 19 23:16:52 2026 +0000
    
        Bump com.sk89q.worldguard:worldguard-bukkit
    
        Bumps com.sk89q.worldguard:worldguard-bukkit from 7.0.18-SNAPSHOT to 7.0.19-SNAPSHOT.
    
        ---
        updated-dependencies:
        - dependency-name: com.sk89q.worldguard:worldguard-bukkit
          dependency-version: 7.0.19-SNAPSHOT
          dependency-type: direct:production
          update-type: version-update:semver-patch
        ...
    
        Signed-off-by: dependabot[bot] <support@github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -90,10 +90,10 @@ dependencies {
     compileOnly("com.sk89q.worldedit:worldedit-bukkit:7.4.6-SNAPSHOT") {
         isTransitive = false
     }
-    compileOnly("com.sk89q.worldguard:worldguard-core:7.0.18-SNAPSHOT") {
+    compileOnly("com.sk89q.worldguard:worldguard-core:7.0.19-SNAPSHOT") {
         isTransitive = false
     }
-    compileOnly("com.sk89q.worldguard:worldguard-bukkit:7.0.18-SNAPSHOT") {
+    compileOnly("com.sk89q.worldguard:worldguard-bukkit:7.0.19-SNAPSHOT") {
         isTransitive = false
     }
     compileOnly("net.citizensnpcs:citizensapi:2.0.43-SNAPSHOT") {
